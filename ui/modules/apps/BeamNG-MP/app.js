3c73e8ceaf0be6c324559e441cc41f4cff96d843
Starystars67 <starystars67@gmail.com>
2019-09-03 16:56:41 +0100

    for some reason these did not commit

--- ui/modules/apps/BeamNG-MP/app.js
+++ ui/modules/apps/BeamNG-MP/app.js
@@ -1,79 +1,99 @@
 angular.module('beamng.apps')
-
-.directive('multiplayer', ['bngApi', function (bngApi) {
+.directive('multiplayer', ['UiUnits', function (UiUnits) {
 	return {
 		templateUrl: 'modules/apps/BeamNG-MP/app.html',
 		replace: true,
-		restrict: 'E',
-		scope: false,
-		controller: function ($scope, $element, $attrs) {
-			$scope.checkImg = "url(modules/apps/BeamNG-MP/checkmark.png)";
+		restrict: 'EA',
+		scope: true,
+		controller: ['$log', '$scope', 'bngApi', function ($log, $scope, bngApi) {
 			$scope.mainStyle = {appWidth: "100%", appHeight: "100%", inLeft: "auto", inRight: "4px", appShow: "visible", groupsShow: "hidden"};
-			$scope.checkStyle = {awareness: $scope.checkImg, method: ""};
 			$scope.visible = {main: true};
-			$scope.inSymbol = "-";
-			$scope.joinOptions = [
-				{port: 1001, ip: "192.168.0.1"}
-			];
-			$scope.ChatMessage = "";
-			$scope.Nickname = "";
 
 			$scope.enterMouse = function() {
-					bngApi.engineLua('setCEFFocus(true)');
+				bngApi.engineLua('setCEFFocus(true)');
+			};
+
+			$scope.init = function() {
+				console.log('UI Loaded');
+				bngApi.engineLua('UI.ready()');
 			}
 
-			this.init = function() {
-				var command = `extensions.freeroam_multiplayer.ready()`;
-				bngApi.engineLua(command);
+			$scope.chatSend = function() {
+				var msg = "ChatSend Pressed.";
+				bngApi.engineLua('UI.console("'+ String(msg) + '")');
+				var cm = document.getElementById("CHATMESSAGE").value;
+				document.getElementById("CHATMESSAGE").value = '';
+				bngApi.engineLua('UI.chatSend("'+ String(cm) + '")');
 			}
 
-			this.toggleUI = function() {
-				$scope.visible.main = !$scope.visible.main;
-				if($scope.visible.main == false) {
-					$scope.mainStyle = {appWidth: "24px", appHeight: "24px", inLeft: "4px", inRight: "auto", appShow: "hidden", groupsShow: "hidden"};
-					$scope.inSymbol = "+";
-				} else {
-					$scope.mainStyle = {appWidth: "100%", appHeight: "100%", inLeft: "auto", inRight: "4px", appShow: "visible", groupsShow: "hidden"};
-					$scope.inSymbol = "-";
+			$scope.joinSession = function() {
+				console.log("PRESSED")
+				var ip = document.getElementById("IP").value;
+				var port = document.getElementById("PORT").value;
+				var nickname = document.getElementById("NICKNAME").value;
+				var ready = true;
+
+				if(port === "") {
+				   console.log('Input is not alphanumeric setting default value 4444');
+				   port = 4444
+				}
+
+				if (ip === "") {
+					console.log('Input IP is empty setting default value localhost')
+					ip = "localhost"
+				}
+
+				if (nickname.length < 3) {
+					console.log('Nickname is too short. At least 3 characters are required.')
+					bngApi.engineLua('UI.error("Your nickname is too short, min of 3 characters.")')
+					document.getElementById("NICKNAME").style.color = "red";
+					document.getElementById("statusText").innerHTML = "Status: nickname too short (3 chars min)"
+					ready = false;
+				}
+
+				if (nickname.length > 30) {
+					console.log('Nickname is too long. A maximum of 30 characters is allowed.')
+					bngApi.engineLua('UI.error("Your nickname is too long, max 30 characters.")')
+					document.getElementById("NICKNAME").style.color = "red";
+					document.getElementById("statusText").innerHTML = "Status: nickname too long (30 chars max)"
+					ready = false;
+				}
+
+				if (nickname.includes("\\")) {
+					console.log('Your nickname contain illegal character.')
+					bngApi.engineLua('UI.error("Your nickname contain illegal character.")')
+					document.getElementById("NICKNAME").style.color = "red";
+					document.getElementById("statusText").innerHTML = "Status: nickname can't contain backslash ( \\ )"
+					ready = false;
+				}
+
+				if (nickname.includes("\"")) {
+					console.log('Your nickname contain illegal character.')
+					bngApi.engineLua('UI.error("Your nickname contain illegal character.")')
+					document.getElementById("NICKNAME").style.color = "red";
+					document.getElementById("statusText").innerHTML = "Status: nickname can't contain quotes ( \" )"
+					ready = false;
+				}
+
+				if (nickname.includes("+")) {
+					console.log('Your nickname contain illegal character.')
+					bngApi.engineLua('UI.error("Your nickname contain illegal character.")')
+					document.getElementById("NICKNAME").style.color = "red";
+					document.getElementById("statusText").innerHTML = "Status: nickname can't contain plus ( + )"
+					ready = false;
+				}
+
+				if (ready) {
+					document.getElementById("NICKNAME").style.color = "black";
+					bngApi.engineLua('UI.setNickname("'+ String(nickname) + '")')
+					bngApi.engineLua('UI.joinSession("'+ String(ip) + '",' + parseInt(port) + ')')
 				}
 			}
 
-			this.setPort = function() {
-				var command = `extensions.freeroam_multiplayer.setPort(${$scope.joinOptions.port})`;
+			$scope.hostSession = function() {
+				var command = `UI.hostSession()`;
 				bngApi.engineLua(command);
 			}
-
-			this.setIP = function() {
-				var command = `extensions.freeroam_multiplayer.setIP(${$scope.joinOptions.ip})`;
-				bngApi.engineLua(command);
-			}
-
-			this.setNickname = function() {
-				var command = `extensions.freeroam_multiplayer.setNickname(${bngApi.serializeToLua({data: this.Nickname})})`;
-				bngApi.engineLua(command);
-			}
-
-			this.chatSend = function() {
-				//console.log("1 > "+this.ChatMessage);
-				bngApi.engineLua(`freeroam_multiplayer.chatSend(${bngApi.serializeToLua({data: this.ChatMessage})})`);
-				this.ChatMessage = "";
-			}
-
-			this.setChatMessage = function() {
-				//console.log("2 > "+this.ChatMessage);
-				bngApi.engineLua(`freeroam_multiplayer.setChatMessage(${bngApi.serializeToLua({data: this.ChatMessage})})`);
-			}
-
-			this.joinSession = function() {
-				var command = `extensions.freeroam_multiplayer.joinSession()`;
-				bngApi.engineLua(command);
-			}
-
-			this.hostSession = function() {
-				var command = `extensions.freeroam_multiplayer.hostSession()`;
-				bngApi.engineLua(command);
-			}
-		},
-		controllerAs: 'ctrl'
-	};
+		}]
+	}
 }]);

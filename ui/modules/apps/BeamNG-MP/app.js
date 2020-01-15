4febb7b88a9287d29fa5f4301046d1c79d91c2dd
Антон <KLiF249@gmail.com>
2020-01-15 19:38:45 +0500

    code cleanup

--- ui/modules/apps/BeamNG-MP/app.js
+++ ui/modules/apps/BeamNG-MP/app.js
@@ -12,31 +12,31 @@ app.directive('multiplayer', ['UiUnits', function (UiUnits) {
 app.controller("Servers", ['$scope', 'bngApi', function ($scope, bngApi) {
 	$scope.warnVis = false;
 	$scope.timer = null;
-	var count = 0;
-	var countUpdateInterval = null;
-	var spamLocked = false;
+	let count = 0;
+	let countUpdateInterval = null;
+	let spamLocked = false;
 
 	$scope.init = function() {
 		bngApi.engineLua('UI.ready("MP")');
 		countUpdateInterval = setInterval(updateCount, 20000);
-	}
+	};
 
 	$scope.reset = function() {
 		connected = false;
 		players = [];
-		init();
-	}
+		$scope.init();
+	};
 
 	$scope.select = function() {
 		bngApi.engineLua('setCEFFocus(true)');
-	}
+	};
 
 	$scope.connect = function() {
 		if (!connected) {
-			var inputs = document.getElementById("form").elements;
-			var ip = inputs["IP"].value;
-			var port = inputs["PORT"].value;
-			var nickname = inputs["NICKNAME"].value;
+			let inputs = document.getElementById("form").elements;
+			let ip = inputs["IP"].value;
+			let port = inputs["PORT"].value;
+			let nickname = inputs["NICKNAME"].value;
 			if(port === "") {
 			port = "4444"
 			}
@@ -44,8 +44,8 @@ app.controller("Servers", ['$scope', 'bngApi', function ($scope, bngApi) {
 				ip = "localhost"
 			}
 			if (nickname.length < 3 || nickname.includes("\\") || nickname.includes("\"") || nickname.includes("'") || nickname.includes("+")) {
-				console.log('Nickname must be longer than 3 chars, cannot contain backslashes, quotes or a plus sign!')
-				bngApi.engineLua('UI.error("Illegal nickname.")')
+				console.log('Nickname must be longer than 3 chars, cannot contain backslashes, quotes or a plus sign!');
+				bngApi.engineLua('UI.error("Illegal nickname.")');
 				addWarning();
 			}
 			else {
@@ -54,30 +54,30 @@ app.controller("Servers", ['$scope', 'bngApi', function ($scope, bngApi) {
 				connected = true;
 			}
 		}
-	}
+	};
 
 	$scope.disconnect = function() {
 		bngApi.engineLua('Network.disconnectFromServer()');
 		setPing("");
 		connected = false;
-	}
+	};
 
 	$scope.select = function() {
 		bngApi.engineLua('setCEFFocus(true)');
-	}
+	};
 
 	$scope.chatSend = function() {
 		if (count > 12 || spamLocked) {
 			spamLimit();
 		}
 		else {
-			var cm = document.getElementById("CHATMESSAGE");
+			let cm = document.getElementById("CHATMESSAGE");
 			if (cm.value) {
 				if (cm.value.length > 150) {
 					msgTooLong();
 				}
 				else {
-					var msg = "ChatSend Pressed.";
+					let msg = "ChatSend Pressed.";
 					bngApi.engineLua('UI.console("'+ String(msg) + '")');
 					bngApi.engineLua('UI.chatSend("'+ String(cm.value) + '")');
 					cm.value = '';
@@ -118,7 +118,7 @@ app.controller("Servers", ['$scope', 'bngApi', function ($scope, bngApi) {
 	function addWarning() {
 		if (!$scope.warnVis) {
 			$scope.warnVis = true;
-			timer = setTimeout(function() {
+			$scope.timer = setTimeout(function() {
 				$scope.warnVis = false;
 			}, 10000)
 		}
@@ -138,10 +138,10 @@ function setDisconnect() {
 }
 
 function addMessage(msg) {
-	var node = document.createElement("li");
+	let node = document.createElement("li");
 	node.style.marginBottom = "4px";
-	var textnode = document.createTextNode(msg);
-	var chat = document.getElementById("CHAT")
+	let textnode = document.createTextNode(msg);
+	let chat = document.getElementById("CHAT");
 	node.appendChild(textnode);
 	chat.appendChild(node);
 
@@ -159,14 +159,15 @@ function greeting(server) {
 
 function playerList(list) {
 	clearPlayerList();
-	var parsedList = JSON.parse(list);
-	var playersList = document.getElementById("players");
-	for (var i = 0; i < parsedList.length; i++) {
+	let parsedList = JSON.parse(list);
+	let playersList = document.getElementById("players");
+	for (let i = 0; i < parsedList.length; i++) {
 		var node = document.createElement("span");
-		if (i==parsedList.length-1) {
-			var text = document.createTextNode(parsedList[i].nickname);
+		let text;
+		if (i===parsedList.length-1) {
+			text = document.createTextNode(parsedList[i].nickname);
 		} else {
-			var text = document.createTextNode(parsedList[i].nickname+", ");
+			text = document.createTextNode(parsedList[i].nickname+", ");
 		}
 		node.appendChild(text);
 		playersList.appendChild(node);
@@ -174,16 +175,16 @@ function playerList(list) {
 }
 
 function clearPlayerList() {
-	var playersList = document.getElementById("players");
+	let playersList = document.getElementById("players");
 	while (playersList.children.length > 0) {
 		playerList.removeChild(playerList.children[0]);
 	}
 }
 
 function setOfflineInPlayerList() {
-	var playersList = document.getElementById("players");
-	var node = document.createElement("span");
-	var text = document.createTextNode("OFFLINE");
+	let playersList = document.getElementById("players");
+	let node = document.createElement("span");
+	let text = document.createTextNode("OFFLINE");
 	node.appendChild(text);
 	playersList.appendChild(node);
 }

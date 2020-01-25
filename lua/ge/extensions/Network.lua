ca36d11ec51b37dea85551f86366ff9b9c764e71
Антон <KLiF249@gmail.com>
2020-01-25 07:54:54 +0500

    env syncing + code cleanup + net working as intended

--- lua/ge/extensions/Network.lua
+++ lua/ge/extensions/Network.lua
@@ -57,35 +57,6 @@ local function disconnectFromServer()
 end
 --====================== DISCONNECT FROM SERVER ======================
 
-local function serializeTable(val, name, skipnewlines, depth)
-	skipnewlines = skipnewlines or false
-	depth = depth or 0
-
-	local tmp = string.rep(" ", depth)
-
-	if name then tmp = tmp .. name .. " = " end
-
-	if type(val) == "table" then
-		tmp = tmp .. "{" .. (not skipnewlines and "\n" or "")
-
-		for k, v in pairs(val) do
-			tmp =  tmp .. serializeTable(v, k, skipnewlines, depth + 1) .. "," .. (not skipnewlines and "\n" or "")
-		end
-
-		tmp = tmp .. string.rep(" ", depth) .. "}"
-	elseif type(val) == "number" then
-		tmp = tmp .. tostring(val)
-	elseif type(val) == "string" then
-		tmp = tmp .. string.format("%q", val)
-	elseif type(val) == "boolean" then
-		tmp = tmp .. (val and "true" or "false")
-	else
-		tmp = tmp .. "\"[inserializeable datatype:" .. type(val) .. "]\""
-	end
-
-	return tmp
-end
-
 --============================================= ON CONNECTED ===============================================
 local function onConnected() -- Function called only 1 time when client successfully connect to server
 	UI.setStatus("Connected") -- TCP connected

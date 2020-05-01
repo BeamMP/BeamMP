eb79520b53145e14865cdcc9b4c869ee51696c8a
Starystars67 <starystars67@gmail.com>
2020-05-01 22:53:56 +0100

    Adding chat

--- ui/modules/apps/BeamNG-MP-Chat/app.js
+++ ui/modules/apps/BeamNG-MP-Chat/app.js
@@ -1,15 +1,13 @@
-var connected = false;
-var players = [];
 var app = angular.module('beamng.apps');
-app.directive('multiplayer', ['UiUnits', function (UiUnits) {
+app.directive('multiplayerchat', ['UiUnits', function (UiUnits) {
 	return {
-		templateUrl: 'modules/apps/BeamNG-MP/app.html',
+		templateUrl: 'modules/apps/BeamNG-MP-Chat/app.html',
 		replace: true,
 		restrict: 'EA',
 		scope: true
 	}
 }]);
-app.controller("Servers", ['$scope', 'bngApi', function ($scope, bngApi) {
+app.controller("Chat", ['$scope', 'bngApi', function ($scope, bngApi) {
 	$scope.warnVis = false;
 	$scope.timer = null;
 	let count = 0;
@@ -17,7 +15,7 @@ app.controller("Servers", ['$scope', 'bngApi', function ($scope, bngApi) {
 	let spamLocked = false;
 
 	$scope.init = function() {
-		bngApi.engineLua('UI.ready("MP")');
+		bngApi.engineLua('UI.ready("CHAT")');
 		countUpdateInterval = setInterval(updateCount, 20000);
 	};
 
@@ -25,42 +23,6 @@ app.controller("Servers", ['$scope', 'bngApi', function ($scope, bngApi) {
 		connected = false;
 		players = [];
 		$scope.init();
-	};
-
-	$scope.select = function() {
-		bngApi.engineLua('setCEFFocus(true)');
-	};
-
-	$scope.connect = function() {
-		if (!connected) {
-			let inputs = document.getElementById("form").elements;
-			let ip = inputs["IP"].value;
-			let port = inputs["PORT"].value;
-			let nickname = inputs["NICKNAME"].value;
-			if(port === "") {
-			port = "4444"
-			}
-			if (ip === "") {
-				ip = "localhost"
-			}
-			if (nickname.length < 3 || nickname.includes("\\") || nickname.includes("\"") || nickname.includes("'") || nickname.includes("+")) {
-				console.log('Nickname must be longer than 3 chars, cannot contain backslashes, quotes or a plus sign!');
-				bngApi.engineLua('UI.error("Illegal nickname.")');
-				addWarning();
-			}
-			else {
-				console.log("TEST" + nickname);
-				bngApi.engineLua('UI.setNickname("'+ nickname.substring(0, 30) + '")');
-				bngApi.engineLua('UI.joinSession("'+ ip + '","' + port + '")');
-				connected = true;
-			}
-		}
-	};
-
-	$scope.disconnect = function() {
-		bngApi.engineLua('Network.disconnectFromServer()');
-		setPing("");
-		connected = false;
 	};
 
 	$scope.select = function() {
@@ -128,18 +90,6 @@ app.controller("Servers", ['$scope', 'bngApi', function ($scope, bngApi) {
 	}
 }]);
 
-function setPing(ping) {
-	document.getElementById("PING").innerHTML = ping;
-}
-
-function setStatus(status) {
-	document.getElementById("STATUS").innerHTML = status;
-}
-
-function setDisconnect() {
-	connected = false;
-}
-
 function addMessage(msg) {
 	//getting current time and adding it to the message before displaying
 	var now = new Date();
@@ -176,53 +126,4 @@ function chatClear() {
 function greeting(server) {
 	addMessage("You are now connected to " + server);
 	addMessage("Welcome to chat! Our message length limit is 150 characters.");
-}
-
-function playerList(list) {
-	clearPlayerList();
-	let playersList = document.getElementById("playerstable");
-	let parsedList = JSON.parse(list);
-
-	for (let i = 0; i < parsedList.length; i++) {
-		var row = playersList.insertRow(playersList.rows.length);
-		var cell1 = row.insertCell(0);
-		cell1.textContent = parsedList[i];
-	}
-
-	if(players != null){
-		//get differences between playernames and send them as messages
-
-		var left = players.filter((item)=>{
-			return !parsedList.includes(item)
-		});
-
-		let joined = parsedList.filter((item)=>{
-			return !players.includes(item)
-		});
-
-		for(var i = 0; i< left.length;i++)
-			if(left[i].length>0) addMessage(left[i] + " left the server"); //the game would send an empty left message when joining a server
-
-		for(var i = 0; i< joined.length;i++)
-			addMessage(joined[i] + " joined the server");
-	}
-	players = parsedList; //store player list as an array for the next update
-}
-
-
-function clearPlayerList() {
-	//players = [];
-	let playersList = document.getElementById("playerstable");
-	var rowCount = playersList.rows.length - 1;
-	for(rowCount; rowCount >= 0; rowCount--) {
-		playersList.deleteRow(rowCount);
-	}
-	var row = playersList.insertRow(0);
-	var cell1 = row.insertCell(0);
-	cell1.textContent = "PLAYERS";
-}
-
-function setOfflineInPlayerList() {
-	let playersList = document.getElementById("players");
-	if(playersList != null) playersList.textContent = "OFFLINE"; //added this cause it was throwing console errors
 }

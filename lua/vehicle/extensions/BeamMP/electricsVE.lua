740b7037cc8421da9ce32c34a7c67ba3da83223f
Starystars67 <starystars67@gmail.com>
2020-05-22 14:17:23 +0100

    Experimental New Electrics System

--- lua/vehicle/extensions/BeamMP/electricsVE.lua
+++ lua/vehicle/extensions/BeamMP/electricsVE.lua
@@ -10,6 +10,7 @@ local M = {}
 
 
 -- ============= VARIABLES =============
+local le = -1
 local slMem = -1
 local srMem = -1
 local hzMem = -1
@@ -28,7 +29,7 @@ local latestData
 -- TODO : Change "allowed" thing and use if player own vehicle don't send any data instead
 
 local function onUpdate(dt) --ONUPDATE OPEN
-	local e = electrics.values
+	--[[local e = electrics.values
 	if sendNow == true or e.signal_left_input ~= slMem or e.signal_right_input ~= srMem or e.hazard_enabled ~= hzMem or e.lights_state ~= lsMem or e.lightbar ~= lbMem or e.horn ~= hrMem or e.extend ~= exMem or e.tilt ~= tiMem or e.tailgate ~= taMem then
 		local eTable = {}
 		eTable[1] = e.signal_left_input   -- Left signal input
@@ -52,7 +53,12 @@ local function onUpdate(dt) --ONUPDATE OPEN
 	hrMem = e.horn
 	exMem = e.extend
 	tiMem = e.tilt
-	taMem = e.tailgate
+	taMem = e.tailgate]]
+	local e = electrics.values
+	if sendNow == true or e ~= le then
+		obj:queueGameEngineLua("electricsGE.sendElectrics(\'"..jsonEncode(e).."\', \'"..obj:getID().."\')") -- Send it to GE lua
+		le = e
+	end
 
 	if sendGearNow == true or e.gearIndex ~= gearMem then
 		obj:queueGameEngineLua("electricsGE.sendGear(\'"..e.gearIndex.."\', \'"..obj:getID().."\')") -- Send it to GE lua
@@ -90,31 +96,36 @@ local function applyElectrics(data)
 	-- 6 = horn
 	local decodedData = jsonDecode(data) -- Decode received data
 		local e = electrics.values
-		if (decodedData) then -- If received data is correct
-		if decodedData[3] ~= e.hazard_enabled then -- Apply hazard lights
+		--[[if (decodedData) then -- If received data is correct
+		if decodedData[3] ~= e.hazard_enabled and decodedData[3] ~= nil then -- Apply hazard lights
 			electrics.set_warn_signal(decodedData[3])
 			electrics.update(0) -- Update electrics values
 		end
-		if e.signal_left_input  ~= decodedData[1] then -- Apply left signal value
+		if e.signal_left_input  ~= decodedData[1] and decodedData[1] ~= nil then -- Apply left signal value
 			electrics.toggle_left_signal()
 			electrics.update(0) -- Update electrics values
 		end
-		if e.signal_right_input ~= decodedData[2] then -- Apply right signal value
+		if e.signal_right_input ~= decodedData[2] and decodedData[2] ~= nil then -- Apply right signal value
 			electrics.toggle_right_signal()
 		end
-		if e.extend ~= decodedData[7] then
+		if e.extend ~= decodedData[7] and decodedData[7] ~= nil then
 			electrics.values.extend = decodedData[7]
 		end
-		if e.tilt ~= decodedData[8] then
+		if e.tilt ~= decodedData[8] and decodedData[8] ~= nil then
 			electrics.values.tilt = decodedData[8]
 		end
-		if e.tailgate ~= decodedData[9] then
+		if e.feet ~= decodedData[10] and decodedData[10] ~= nil then
+			print("Setting Tailgate to: "..decodedData[10].." from "..electrics.values.tailgate)
+			electrics.values.tailgate = decodedData[10]
+		end
+		if e.tailgate ~= decodedData[9] and decodedData[9] ~= nil then
+			print("Setting Tailgate to: "..decodedData[9].." from "..electrics.values.tailgate)
 			electrics.values.tailgate = decodedData[9]
 		end
-		if e.lightbar ~= decodedData[4] then
+		if e.lights_state ~= decodedData[4] and decodedData[4] ~= nil then
 			electrics.setLightsState(decodedData[4]) -- Apply lights values
 		end
-		if e.lights_state ~= decodedData[4] then
+		if e.lightbar ~= decodedData[5] and decodedData[5] ~= nil then
 			electrics.set_lightbar_signal(decodedData[5]) -- Apply lightbar values
 		end
 		-- Apply horn value
@@ -122,6 +133,9 @@ local function applyElectrics(data)
 			electrics.horn(true)
 		elseif decodedData[6] == 0 and e.horn == 1 then
 			electrics.horn(false)
+		end]]
+		for k,v in pairs(decodedData) do
+			print("Setting: "..k.." -> "..v)
 		end
 		latestData = data
 	end

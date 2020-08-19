7a0ac33cfcfe44bb9b91450598ee99427b34c358
stefan750 <7531670+stefan750@users.noreply.github.com>
2020-08-19 19:36:02 +0200

    Velocity improvements
    No longer applies force to detached parts
    Damaged cars no longer start vibrating
    Minor performance optimizations

--- lua/vehicle/extensions/BeamMP/velocityVE.lua
+++ lua/vehicle/extensions/BeamMP/velocityVE.lua
@@ -3,19 +3,86 @@
 
 local M = {}
 
+local connectedBeams = {}
+local isConnectedNode = {}
+local beamsChanged = false
+local physicsFPS = 0
+
+-- Recursively find nodes connected to parent node
+local function findConnectedNodesRecursive(parentID)
+	if isConnectedNode[parentID] then return end
+	
+	isConnectedNode[parentID] = true
+	
+	-- Apparently normal for loop is twice as fast as ipairs()
+	local beams = connectedBeams[parentID]
+	for i=1, #beams do
+		local bid = beams[i]
+		if not obj:beamIsBroken(bid) then
+			local b = v.data.beams[bid]
+			
+			if parentID == b.id1 then
+				findConnectedNodesRecursive(b.id2)
+			elseif parentID == b.id2 then
+				findConnectedNodesRecursive(b.id1)
+			end
+		end
+	end
+end
+
+local function findConnectedNodes()
+	isConnectedNode = {}
+	findConnectedNodesRecursive(v.data.refNodes[0].ref)
+	
+	beamsChanged = false
+end
+
+local function onInit()
+	physicsFPS = obj:getPhysicsFPS() or 2000
+	
+	-- Store connected beams for each node
+	for _, b in pairs(v.data.beams) do
+		if b.beamType ~= 7 then -- exclude type BEAM_SUPPORT
+			if connectedBeams[b.id1] == nil then 
+				connectedBeams[b.id1] = {}
+			end
+			if connectedBeams[b.id2] == nil then 
+				connectedBeams[b.id2] = {}
+			end
+			table.insert(connectedBeams[b.id1], b.cid)
+			table.insert(connectedBeams[b.id2], b.cid)
+		end
+	end
+	
+	-- TODO: find less hacky way to get beamBroke events
+	local beamBroke = powertrain.beamBroke
+	powertrain.beamBroke = function(id, ...)
+		beamsChanged = true
+		
+		return beamBroke(id, ...)
+	end
+	
+	findConnectedNodes()
+	
+	print("velocityVE init, physicsFPS: "..physicsFPS)
+end
+
 -- Add velocity to vehicle in m/s
 -- How it works: Apply enough force to each node, so it accelerates to the target speed in 1 physics tick.
 --               Because all nodes accelerate at the same rate, the vehicle will not get ripped apart
--- NOTE: - also affects parts that are detached from the car
---       - very high values can cause instability
+-- NOTE: - very high values can cause instability
 local function addVelocity(x, y, z)
+	if beamsChanged then
+		findConnectedNodes()
+	end
+	
 	local vel = vec3(x, y, z)
 	--print("addVelocity: x: "..x..", y: "..y..", z: "..z)
-	for _, node in pairs(v.data.nodes) do
-		local nodeWeight = obj:getNodeMass(node.cid)
-		local forceVec = vel*nodeWeight*2000 -- calculate force for desired acceleration
+	for nid, connected in pairs(isConnectedNode) do
+		local nodeWeight = obj:getNodeMass(nid)
+		local forceVec = vel*nodeWeight*physicsFPS -- calculate force for desired acceleration
 			
-		obj:applyForceVector(node.cid, forceVec:toFloat3())
+		obj:applyForceVector(nid, forceVec:toFloat3())
 	end
 end
 
@@ -32,31 +99,28 @@ end
 -- pos roll makes it roll from left to right
 -- pos pitch makes the nose go up
 
---pitch is pos or growing when the nose is going up
---roll is decreasing from left to right
---yaw goes negative from left to right
-
 -- Add angular velocity to vehicle in rad/s
 -- How it works: Calculate node tangential velocity relative to car center point at the desired angular velocity
 --               and apply enough force to reach the calculated speed in 1 physics tick.
 -- NOTE: - will rotate around vehicle position, not center of gravity (calculated COG moves with detached parts)
 --         so can cause slight linear movement in some cases
---       - also affects parts that are detached from the car
 --       - very high values can destroy vehicles (above about 20-30 rad/s for most cars) or cause instability
---       - can become inaccurate if vehicles are very deformed
 local function addAngularVelocity(x, y, z, pitchAV, rollAV, yawAV)
+	if beamsChanged then
+		findConnectedNodes()
+	end
+	
 	local vel = vec3(x, y, z)
-	local av = vec3(pitchAV, rollAV, yawAV)
 	local toWorldAxisQuat = quat(obj:getRotation())
+	local av = vec3(pitchAV, rollAV, yawAV):rotated(toWorldAxisQuat)
 	--print("addAngularVelocity: pitchAV: "..pitchAV..", rollAV: "..rollAV..", yawAV: "..yawAV)
-	for _, node in pairs(v.data.nodes) do
-		local nodeWeight = obj:getNodeMass(node.cid)
-		local nodePos = vec3(node.pos)
-		local localTargetAcc = nodePos:cross(av) -- TODO: might be inaccurate for large vehicles
-		local targetAcc = vel + localTargetAcc:rotated(toWorldAxisQuat) -- rotate force vector to world axis
-		local forceVec = targetAcc*nodeWeight*2000 -- calculate force for desired acceleration
+	for nid, connected in pairs(isConnectedNode) do
+		local nodeWeight = obj:getNodeMass(nid)
+		local nodePos = vec3(obj:getNodePosition(nid))
+		local targetAcc = vel + nodePos:cross(av)
+		local forceVec = targetAcc*nodeWeight*physicsFPS -- calculate force for desired acceleration
 		
-		obj:applyForceVector(node.cid, forceVec:toFloat3())
+		obj:applyForceVector(nid, forceVec:toFloat3())
 	end
 end
 
@@ -74,8 +138,10 @@ local function setAngularVelocity(x, y, z, pitchAV, rollAV, yawAV)
 end
 
 -- public interface
-M.addVelocity = addVelocity
-M.setVelocity = setVelocity
+M.onInit             = onInit
+M.onExtensionLoaded  = onInit
+M.addVelocity        = addVelocity
+M.setVelocity        = setVelocity
 M.addAngularVelocity = addAngularVelocity
 M.setAngularVelocity = setAngularVelocity
 

2e7c6f8605300bbe8eed1ee0ed6b24e025308175
jojos38 <jojos38000@hotmail.com>
2020-12-25 01:21:41 +0100

    Fixed mods backup and reload
    Fixed mods backup and reload

--- lua/ge/extensions/MPCoreNetwork.lua
+++ lua/ge/extensions/MPCoreNetwork.lua
@@ -322,10 +322,6 @@ local function onClientStartMission(mission)
 		print("The user has loaded another mission!")
 		Lua:requestReload()
 	end
-	if isMPSession() then
-		MPModManager.backupLoadedMods() -- Backup the current loaded mods
-		MPModManager.checkAllMods() -- Checking all the mods
-	end
 	-- Checking all the mods again because BeamNG.drive have a bug with mods not deactivating
 end
 

bbb75e18971e041726f7d48ac8786bb098fc58cb
Deer McDurr <doni@doni.hu>
2021-03-29 20:38:47 +0200

    moved network debug to the proper place

--- lua/ge/extensions/MPGameNetwork.lua
+++ lua/ge/extensions/MPGameNetwork.lua
@@ -47,6 +47,7 @@ local function sendData(s)
 	if settings.getValue("showDebugOutput") == true then
 		print('[MPGameNetwork] Sending Data ('..r..'): '..s)
 	end
+	MPDebug.packetSent(r)
 end
 
 
@@ -126,6 +127,8 @@ local function onUpdate(dt)
 			local code = string.sub(received, 1, 1)
 			local data = string.sub(received, 2)
 			HandleNetwork[code](data)
+
+			MPDebug.packetReceived(string.len(received))
 		end
 	end
 end

5efa95bf229bb33e904c7de6d8832fe8858ed723
Starystars67 <starystars67@gmail.com>
2022-06-15 20:20:44 +0100

    Fix for loading screen freeze issue.

--- lua/ge/extensions/multiplayer/multiplayer.lua
+++ lua/ge/extensions/multiplayer/multiplayer.lua
@@ -5,7 +5,6 @@ log("I", "", "Gamemode Loaded")
 
 local inputActionFilter = extensions.core_input_actionFilter
 
-local readyCalled = false
 local originalGetDriverData = nop
 local originalToggleWalkingMode = nop
 
@@ -30,30 +29,31 @@ local function startMultiplayerHelper (level, startPointName)
 end
 
 local function startAssociatedFlowgraph(level)
--- load flowgaphs associated with this level.
-	if level.flowgraphs then
-		for _, absolutePath in ipairs(level.flowgraphs or {}) do
-			local relativePath = level.misFilePath..absolutePath
-			local path = FS:fileExists(absolutePath) and absolutePath or (FS:fileExists(relativePath) and (relativePath) or nil)
-			if path then
-				local mgr = core_flowgraphManager.loadManager(path)
-				core_flowgraphManager.startOnLoadingScreenFadeout(mgr)
-				mgr.stopRunningOnClientEndMission = true -- make mgr self-destruct when level is ended.
-				mgr.removeOnStopping = true -- make mgr self-destruct when level is ended.
-				log("I", "Flowgraph loading", "Loaded level-associated flowgraph from file "..dumps(path))
-			 else
-				 log("E", "Flowgraph loading", "Could not find file in either '" .. absolutePath.."' or '" .. relativePath.."'!")
-			 end
+	-- load flowgaphs associated with this level.
+		if level.flowgraphs then
+			for _, absolutePath in ipairs(level.flowgraphs or {}) do
+				local relativePath = level.misFilePath..absolutePath
+				local path = FS:fileExists(absolutePath) and absolutePath or (FS:fileExists(relativePath) and (relativePath) or nil)
+				if path then
+					local mgr = core_flowgraphManager.loadManager(path)
+					--core_flowgraphManager.startOnLoadingScreenFadeout(mgr)
+					mgr:setRunning(true)
+					mgr.stopRunningOnClientEndMission = true -- make mgr self-destruct when level is ended.
+					mgr.removeOnStopping = true -- make mgr self-destruct when level is ended.
+					log("I", "Flowgraph loading", "Loaded level-associated flowgraph from file "..dumps(path))
+				 else
+					 log("E", "Flowgraph loading", "Could not find file in either '" .. absolutePath.."' or '" .. relativePath.."'!")
+				 end
+			end
 		end
 	end
-end
 
 local function startMultiplayer(level, startPointName, wasDelayed)
 	core_gamestate.requestEnterLoadingScreen(logTag)
 	-- if this was a delayed start, load the FGs now.
-	if wasDelayed then
-		startAssociatedFlowgraph(level)
-	end
+	--if wasDelayed then
+		--startAssociatedFlowgraph(level)
+	--end
 
 	-- this is to prevent bug where multiplayer is started while a different level is still loaded.
 	-- Loading the new multiplayer causes the current loaded multiplayer to unload which breaks the new multiplayer
@@ -76,14 +76,17 @@ local function startMultiplayer(level, startPointName, wasDelayed)
 	if not wasDelayed and not delaying then
 		startAssociatedFlowgraph(level)
 	end
+
+	if not shipping_build and settings.getValue('enableCrashCam') then
+    extensions.load('core_crashCamMode')
+  end
 end
 
 local function startMultiplayerByName(levelName)
-	for i, v in ipairs(core_levels.getList()) do
-		if v.levelName == levelName then
+  local level = core_levels.getLevelByName(levelName)
+  if level then
 			startMultiplayer(v)
 			return true
-		end
 	end
 	return false
 end
@@ -94,7 +97,6 @@ local function onClientPreStartMission(mission)
 		file = path .. 'mainLevel'
 		if not FS:fileExists(file..'.lua') then return end
 		extensions.loadAtRoot(file,"")
-		core_gamestate.setGameState('multiplayer', 'multiplayer', 'multiplayer') -- This is added to set the UI elements
 		if mainLevel and mainLevel.onClientPreStartMission then
 			mainLevel.onClientPreStartMission(mission)
 		end
@@ -104,8 +106,6 @@ end
 local function onClientPostStartMission()
 	if MPCoreNetwork.isMPSession() then
 	core_gamestate.setGameState('multiplayer', 'multiplayer', 'multiplayer') -- This is added to set the UI elements
-
-	--readyCalled = true
 	MPGameNetwork.connectToLauncher()
 	end
 end
@@ -114,21 +114,16 @@ local function onClientStartMission(mission)
 	if M.state.multiplayerActive then
 		extensions.hook('onMultiplayerLoaded', mission)
 		local ExplorationCheckpoints = scenetree.findObject("ExplorationCheckpointsActionMap")
-		if ExplorationCheckpoints then
-			ExplorationCheckpoints:push()
-		end
+		local am = scenetree.findObject("ExplorationCheckpointsActionMap")
+    if am then am:push() end
 	end
 end
 
 local function onClientEndMission(mission)
-	readyCalled = false
-
 	if M.state.multiplayerActive then
 		M.state.multiplayerActive = false
-		local ExplorationCheckpoints = scenetree.findObject("ExplorationCheckpointsActionMap")
-		if ExplorationCheckpoints then
-			ExplorationCheckpoints:pop()
-		end
+		local am = scenetree.findObject("ExplorationCheckpointsActionMap")
+    if am then am:pop() end
 	end
 
 	if not mainLevel then return end
@@ -141,25 +136,28 @@ end
 -- Resets previous vehicle alpha when switching between different vehicles
 -- Used to fix multipart highlighting when switching vehicles
 local function onVehicleSwitched(oldId, newId, player)
-	if oldId then
-		local veh = be:getObjectByID(oldId)
-		if veh then
-			extensions.core_vehicle_partmgmt.selectReset()
-		end
-	end
+  if oldId then
+    local veh = be:getObjectByID(oldId)
+    if veh then
+      extensions.core_vehicle_partmgmt.selectReset()
+    end
+  end
+  if newId then
+    local veh = be:getObjectByID(newId)
+    if veh then
+      veh:refreshLastAlpha()
+    end
+  end
 end
 
 local function onResetGameplay(playerID)
-	local scenario = scenario_scenarios and scenario_scenarios.getScenario()
-	local campaign = campaign_campaigns and campaign_campaigns.getCampaign()
-	-- check if a flowgraph can consume this action.
-	local fg = nil
-	for _, mgr in ipairs(core_flowgraphManager.getAllManagers()) do
-		fg = fg or mgr:hasNodeForHook('onResetGameplay')
-	end
-	if not scenario and not campaign and not fg then
-		be:resetVehicle(playerID)
-	end
+  if scenario_scenarios and scenario_scenarios.getScenario() then return end
+  if campaign_campaigns and campaign_campaigns.getCampaign() then return end
+  if career_career      and career_career.isEnabled()        then return end
+  for _, mgr in ipairs(core_flowgraphManager.getAllManagers()) do
+    if mgr:blocksOnResetGameplay() then return end
+  end
+  be:resetVehicle(playerID)
 end
 
 local function modifiedGetDriverData(veh)
@@ -195,6 +193,43 @@ local function onUpdate(dt)
 		originalToggleWalkingMode = gameplay_walk.toggleWalkingMode
 		gameplay_walk.toggleWalkingMode = modifiedToggleWalkingMode
 	end
+	
+	if worldReadyState == 0 then
+    -- When the world is ready, we have to set the camera we want to use. However, we want to do this
+    -- when we have vehicles spawned.
+    local vehicles = scenetree.findClassObjects('BeamNGVehicle')
+    for k, vecName in ipairs(vehicles) do
+      local to = scenetree.findObject(vecName)
+      if to and to.obj and to.obj:getId() then
+        commands.setGameCamera()
+        break
+      end
+    end
+  end
+end
+
+local function onAnyActivityChanged(state)
+  if not shipping_build then
+    if state == "started" then
+      if core_crashCamMode then
+        extensions.unload('core_crashCamMode')
+      end
+    elseif state == "stopped" then
+      if settings.getValue('enableCrashCam') then
+        extensions.load('core_crashCamMode')
+      end
+    end
+  end
+end
+
+local function onSettingsChanged()
+  if not shipping_build then
+    if settings.getValue('enableCrashCam') then
+      extensions.load('core_crashCamMode')
+    elseif core_crashCamMode then
+      extensions.unload('core_crashCamMode')
+    end
+  end
 end
 
 -- public interface
@@ -206,7 +241,8 @@ M.onClientStartMission      = onClientStartMission
 M.onClientEndMission        = onClientEndMission
 M.onVehicleSwitched         = onVehicleSwitched
 M.onResetGameplay           = onResetGameplay
-
 M.onUpdate                  = onUpdate
+M.onAnyActivityChanged    = onAnyActivityChanged
+M.onSettingsChanged       = onSettingsChanged
 
 return M

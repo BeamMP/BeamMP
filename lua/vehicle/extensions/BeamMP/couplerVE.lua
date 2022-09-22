83862fa76be4ca75036322dc6cde2a9d2840e1e2
Olrosse <olaemilrossevatn@hotmail.com>
2022-09-22 21:10:26 +0200

    syncing for advanced couplers

--- lua/vehicle/extensions/BeamMP/couplerVE.lua
+++ lua/vehicle/extensions/BeamMP/couplerVE.lua
@@ -7,50 +7,129 @@
 
 local M = {}
 
-
+local timer = 0.5 -- timer to prevent doors from opening on spawn and reset
+local MPcouplercache = {}
 
 local function toggleCouplerState(data)
 	local decodedData = jsonDecode(data)
-	local ID = obj:getID()
-	if decodedData._nodetag and ID == obj2id then
-		if decodedData.state then
-			beamstate.attachCouplers(decodedData._nodetag)
-		else
-			beamstate.detachCouplers(decodedData._nodetag, decodedData.forceLocked, decodedData.forceWelded)
+	for k,v in pairs(decodedData) do
+		if v.state == false or v.state == true then
+			if v._nodetag and not v.trailer then
+				if v.state then
+					beamstate.attachCouplers(v._nodetag)
+				else
+					obj:detachCoupler(v._nodetag, 0)
+				end
+			elseif v.state == true then
+				beamstate.activateAutoCoupling()
+			elseif v.state == false then
+				beamstate.disableAutoCoupling()
+				beamstate.detachCouplers()
+				obj:stopLatching()
+			end
+		elseif controller.getControllerSafe(v.name).getGroupState() ~= v.state then
+			if v.state == "detached" or v.state == "autoCoupling" or v.state == "broken" then
+				controller.getControllerSafe(v.name).detachGroup()
+			elseif v.state == "attached" then
+				controller.getControllerSafe(v.name).tryAttachGroupImpulse()
+			end
 		end
-	elseif decodedData.state then
-		beamstate.activateAutoCoupling()
-	else
-		beamstate.disableAutoCoupling()
-		beamstate.detachCouplers()
-		obj:stopLatching()
 	end
 end
 
 local function onCouplerAttached(nodeId, obj2id, obj2nodeId, attachSpeed, attachEnergy)
-	local MPcouplers = {}
-	MPcouplers.state = true
-	MPcouplers._nodetag = nodeId
-	MPcouplers.obj2id = obj2id
 	if v.mpVehicleType == "L" then
-		obj:queueGameEngineLua("MPVehicleGE.sendBeamstate(\'"..jsonEncode(MPcouplers).."\'," ..tostring(obj:getID())..")")
+		local ID = obj:getID()
+		-- Advanced couplers, doors etc
+		local MPcouplerdata = {}
+		if timer <= 0 and ID == obj2id then
+			for k,v in pairs(MPcouplercache) do
+				local state = controller.getControllerSafe(v.name).getGroupState()
+				if v.state ~= state then
+					local couplerstates = {}
+					couplerstates.name = v.name
+					couplerstates.state = state
+					table.insert(MPcouplerdata,couplerstates)
+				end
+				v.state = state
+			end
+		end
+
+		-- basic couplers
+		local MPcouplers = {}
+		MPcouplers.state = true
+		MPcouplers._nodetag = nodeId
+		if ID == obj2id then -- checking if coupler is connecting to another vehicle
+			MPcouplers.trailer = false
+		else
+			MPcouplers.trailer = true
+		end
+
+		MPcouplers.obj2id = obj2id
+		table.insert(MPcouplerdata,MPcouplers)
+		obj:queueGameEngineLua("MPVehicleGE.sendBeamstate(\'"..jsonEncode(MPcouplerdata).."\'," ..tostring(obj:getID())..")")
 	end
 end
 
 local function onCouplerDetached(nodeId, obj2id, obj2nodeId)
-	local MPcouplers = {}
-	MPcouplers.state = false
-	MPcouplers._nodetag = nodeId
-	MPcouplers.obj2id = obj2id
 	if v.mpVehicleType == "L" then
-		obj:queueGameEngineLua("MPVehicleGE.sendBeamstate(\'"..jsonEncode(MPcouplers).."\'," ..tostring(obj:getID())..")")
+		local ID = obj:getID()
+		-- Advanced couplers, doors etc
+		local MPcouplerdata = {}
+		if timer <= 0 and ID == obj2id then
+			for k,v in pairs(MPcouplercache) do
+				local state = controller.getControllerSafe(v.name).getGroupState()
+				if v.state ~= state then
+					local couplerstates = {}
+					couplerstates.name = v.name
+					couplerstates.state = state
+					table.insert(MPcouplerdata,couplerstates)
+				end
+				v.state = state
+			end
+		end
+
+		-- basic couplers
+		local MPcouplers = {}
+		MPcouplers.state = false
+		MPcouplers._nodetag = nodeId
+		if ID == obj2id then -- checking if coupler is connecting to another vehicle
+			MPcouplers.trailer = false
+		else
+			MPcouplers.trailer = true
+		end
+
+		MPcouplers.obj2id = obj2id
+		table.insert(MPcouplerdata,MPcouplers)
+		obj:queueGameEngineLua("MPVehicleGE.sendBeamstate(\'"..jsonEncode(MPcouplerdata).."\'," ..tostring(obj:getID())..")")
 	end
 end
 
+local function updateGFX(dt)
+	if timer >= 0 then
+		timer = timer - dt
+	end
+end
 
+local function onReset()
+	timer = 0.5
+	MPcouplercache = {}
+	local AdvCouplers = controller.getControllersByType("advancedCouplerControl")
+	if AdvCouplers == nil then return end
+	for k,v in pairs(AdvCouplers) do
+		local couplerstates = {}
+		couplerstates.name = v.name
+		couplerstates.state = "attached"
+		table.insert(MPcouplercache,couplerstates)
+	end
+end
 
+M.onReset            = onReset
+M.onInit             = onReset
+M.onExtensionLoaded  = onReset
 M.toggleCouplerState = toggleCouplerState
-M.onCouplerAttached = onCouplerAttached
-M.onCouplerDetached = onCouplerDetached
+M.onCouplerAttached  = onCouplerAttached
+M.onCouplerDetached  = onCouplerDetached
+M.updateGFX          = updateGFX
 
 return M

09ea3994a62759131cbe9a8aec7e2999640684bb
snepsnepsnep <jaakow22@gmail.com>
2022-10-19 23:57:33 +0200

    update nodesGE

--- lua/ge/extensions/nodesGE.lua
+++ lua/ge/extensions/nodesGE.lua
@@ -9,62 +9,39 @@ local M = {}
 print("Loading nodesGE...")
 
 
-
--- ============= VARIABLES =============
--- ============= VARIABLES =============
-
-
-
 local function tick()
-	local ownMap = MPVehicleGE.getOwnMap() -- Get map of own vehicles
-	for i,v in pairs(ownMap) do -- For each own vehicle
-		local veh = be:getObjectByID(i) -- Get vehicle
+	local ownMap = MPVehicleGE.getOwnMap()
+	for i,v in pairs(ownMap) do
+		local veh = be:getObjectByID(i)
 		if veh then
 			veh:queueLuaCommand("nodesVE.getNodes()")
 		end
 	end
 end
 
-local function sendNodes(data, gameVehicleID) -- Update electrics values of all vehicles - The server check if the player own the vehicle itself
+local function sendNodes(data, gameVehicleID)
 	if MPGameNetwork.launcherConnected() then
-		local serverVehicleID = MPVehicleGE.getServerVehicleID(gameVehicleID) -- Get serverVehicleID
-		if serverVehicleID and MPVehicleGE.isOwn(gameVehicleID) then -- If serverVehicleID not null and player own vehicle
-			MPGameNetwork.sendSplit('Xn:', serverVehicleID..":", data)
+		local serverVehicleID = MPVehicleGE.getServerVehicleID(gameVehicleID)
+		if serverVehicleID and MPVehicleGE.isOwn(gameVehicleID) then
+			MPGameNetwork.send('Xn:'..serverVehicleID..":"..data)
 		end
 	end
 end
 
 local function applyNodes(data, serverVehicleID)
-	local gameVehicleID = MPVehicleGE.getGameVehicleID(serverVehicleID) or -1 -- get gameID
+	local gameVehicleID = MPVehicleGE.getGameVehicleID(serverVehicleID) or -1
 	local veh = be:getObjectByID(gameVehicleID)
 	if veh then
-		--local pos = veh:getPosition()
-		--veh:setPositionRotation(pos.x, pos.y, pos.z, 0, 0, 0.01, math.random())
-		veh:queueLuaCommand("nodesVE.applyNodes(\'"..data.."\')") -- Send nodes values
-	end
-end
-
-local function applyRot(data, serverVehicleID)
-	local gameVehicleID = MPVehicleGE.getGameVehicleID(serverVehicleID) or -1 -- get gameID
-	local veh = be:getObjectByID(gameVehicleID)
-	if veh then
-		veh:queueLuaCommand("nodesVE.applyRotation(\'"..data.."\')") -- Send nodes values
+		veh:queueLuaCommand("nodesVE.applyNodes(\'"..data.."\')")
 	end
 end
 
 local function handle(rawData)
-	--print("nodesGE.handle: "..rawData)
-	local code = string.sub(rawData, 1, 1)
-	if code == "p" then
-		rawData = string.sub(rawData,3)
-		local serverVehicleID = string.match(rawData,"(%w+)%:")
-		local data = string.match(rawData,":(.*)")
-		applyRot(data, serverVehicleID)
-	else
-		rawData = string.sub(rawData,3)
-		local serverVehicleID = string.match(rawData,"(%w+)%:")
-		local data = string.match(rawData,":(.*)")
+	local code, serverVehicleID, data = string.match(rawData, "^(%a)%:(%d+%-%d+)%:({.*})")
+	if code == "n" then
 		applyNodes(data, serverVehicleID)
+	else
+		log('W', 'handle', "Received unknown packet '"..code.."'! ".. rawData)
 	end
 end
 
@@ -74,8 +51,6 @@ M.tick       = tick
 M.handle     = handle
 M.sendNodes  = sendNodes
 M.applyNodes = applyNodes
-M.applyRot   = applyRot
-
 
 
 print("nodesGE loaded")

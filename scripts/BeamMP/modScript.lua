b124d4eb9c204b25af1c5afe3a05d049575cd07c
snepsnepsnep <jaakow22@gmail.com>
2022-12-15 00:11:28 +0100

    unload mod if game ver is incompatible

--- scripts/BeamMP/modScript.lua
+++ scripts/BeamMP/modScript.lua
@@ -2,6 +2,25 @@
 -- All work by Titch2000 and jojos38.
 -- You have no permission to edit, redistribute or upload. Contact us for more info!
 --====================================================================================
+local ver = split(beamng_versionb, ".")
+local majorVer = tonumber(ver[2])
+local compatibleVersion = 27
+if majorVer ~= compatibleVersion then
+	log('W', 'versionCheck', 'BeamMP is incompatible with BeamNG.drive version '..beamng_versionb)
+	log('M', 'versionCheck', 'Deactivating BeamMP mod.')
+	core_modmanager.deactivateMod('multiplayerbeammp')
+	core_modmanager.deactivateMod('beammp')
+	if majorVer > compatibleVersion then
+		guihooks.trigger("toastrMsg", {type="error", title="Error loading BeamMP", msg="BeamMP is currently not compatible with BeamNG.drive version "..beamng_versionb..". Check the BeamMP Discord for updates."})
+		log('W', 'versionCheck', 'BeamMP is currently not compatible with BeamNG.drive version '..beamng_versionb..'. Check the BeamMP Discord for updates.')
+	else
+		guihooks.trigger("toastrMsg", {type="error", title="Error loading BeamMP", msg="BeamMP is not compatible with BeamNG.drive version "..beamng_versionb.. ". Please update your game."})
+		log('W', 'versionCheck', 'BeamMP is not compatible with BeamNG.drive version '..beamng_versionb.. '. Please update your game.')
+	end
+	return
+else
+	log('M', 'versionCheck', 'BeamMP is compatible with the current version.')
+end
 
 load("multiplayer/multiplayer")
 registerCoreModule("multiplayer/multiplayer")

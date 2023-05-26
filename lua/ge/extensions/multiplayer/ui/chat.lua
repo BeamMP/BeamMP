3136fdf45bd853d00c066867aa4b24e4884f39ba
vulcan-dev <vulcan-dev@proton.me>
2023-05-26 23:02:10 +0100

    Set `fadeTime`'s minimum to 0.1

--- lua/ge/extensions/multiplayer/ui/chat.lua
+++ lua/ge/extensions/multiplayer/ui/chat.lua
@@ -1,202 +1,186 @@
--- Note: Colors are not used anymore but I may add them back in the future
-
-local M = {
-    chatMessages = {},
-    newMessageCount = 0
-}
+local M = {}
 
 local utils = require("multiplayer.ui.utils")
-local ffi = require('ffi')
 
 local imgui = ui_imgui
-local heightOffset = 20
-local forceBottom = false
-local scrollToBottom = false
-local chatMessageBuf = imgui.ArrayChar(256)
-local wasMessageSent = false
-local history = {}
-local historyPos = -1
+local longestSettingName = 0
+local sortedSettings = {}
 
-local inputCallbackC = ffi.cast("ImGuiInputTextCallback", function(data)
-    if data.EventFlag == imgui.InputTextFlags_CallbackHistory then
-        local prevHistoryPos = historyPos
-        if data.EventKey == imgui.Key_UpArrow then
-            historyPos = historyPos - 1
-            if historyPos < 1 then
-                if historyPos < 0 then
-                    historyPos = #history
-                else
-                    historyPos = 1
-                end
-            end
-        elseif data.EventKey == imgui.Key_DownArrow then
-            if #history > 0 and historyPos == #history then
-                ffi.fill(data.Buf, data.BufSize, 0)  -- Clear the buffer
-                data.CursorPos = 0
-                data.SelectionStart = 0
-                data.SelectionEnd = 0
-                data.BufTextLen = 0
-                data.BufDirty = imgui.Bool(true)
-                historyPos = -1
-                return imgui.Int(0)  -- Return 0 to prevent further processing
-            elseif historyPos == -1 then -- Empty, not on any history
-                return imgui.Int(0)
-            end
-
-            historyPos = historyPos + 1
-        end
-
-        if #history > 0 and prevHistoryPos ~= historyPos then
-            local t = history[historyPos]
-            if type(t) ~= "string" then return imgui.Int(0) end
-            local inplen = string.len(t)
-            local inplenInt = imgui.Int(inplen)
-            ffi.copy(data.Buf, t, math.min(data.BufSize - 1, inplen + 1))
-            data.CursorPos = inplenInt
-            data.SelectionStart = inplenInt
-            data.SelectionEnd = inplenInt
-            data.BufTextLen = inplenInt
-            data.BufDirty = imgui.Bool(true);
-        end
-    elseif data.EventFlag == imgui.InputTextFlags_CallbackCharFilter and
-        data.EventChar == 96 then -- 96 = '`'
-        return imgui.Int(1)
-    end
-    return imgui.Int(0)
-end)
-
-local function clearHistory()
-    log('I', "BeamMP UI", "Cleared chat history")
-    history = {}
+local function toTitleCase(str)
+    return str:gsub("%u", function(c) return " " .. c end):gsub("^%l", string.upper)
 end
 
-local function sendChatMessage(message)
-    if message[0] == 0 then return end
-
-    message = ffi.string(message)
-    -- local messageTable = {
-    --     message = message,
-    --     sentTime = os.time(),
-    --     id = #M.chatMessages + 1
-    -- }
-
-    local c = 'C:'..MPConfig.getNickname()..": "..message
-	MPGameNetwork.send(c)
-	TriggerClientEvent("ChatMessageSent", c)
-
-    -- table.insert(M.chatMessages, messageTable) -- ! For debugging, remove this line and messageTable (addMessage handles this)
-    wasMessageSent = true
-    history[#history+1] = ffi.string(chatMessageBuf)
-    historyPos = -1
-    ffi.copy(chatMessageBuf, "")
+--------------------------------
+-----[ Utility Functions ]------
+--------------------------------
+local function saveConfig()
+    local jsonData = jsonEncode(UI.settings)
+    local config = io.open("./settings/BeamMP/chat.json", "w")
+    config:write(jsonData)
+    config:close()
 end
 
-local function addMessage(message)
-    local messageTable = {
-        message = message,
-        sentTime = os.time(),
-        id = #M.chatMessages + 1
-    }
-
-    table.insert(M.chatMessages, messageTable)
-
-    local name = messageTable.message:sub(1, messageTable.message:find(':')-1)
-    if not forceBottom and name ~= MPConfig:getNickname() then
-        M.newMessageCount = M.newMessageCount + 1
-    end
-end
-
-local scrollbarVisible = false
-
-local function render()
-    local scrollbarSize = imgui.GetStyle().ScrollbarSize
-
-    if imgui.BeginChild1("ChatArea", imgui.ImVec2(0, -imgui.GetTextLineHeightWithSpacing() - heightOffset), false) then
-        scrollbarVisible = imgui.GetScrollMaxY() > 0
-        local windowWidth = imgui.GetWindowWidth()
-        local scrollbarPos = imgui.GetScrollY()
-
-        if scrollbarPos >= imgui.GetScrollMaxY() then
-            M.newMessageCount = 0
-            wasMessageSent = false
-            forceBottom = true
-        else
-            forceBottom = false
-        end
-
-        -- Render Message | Time
-        for _, message in ipairs(M.chatMessages) do
-            imgui.Columns(2, "ChatColumns", false)
-
-            if not scrollbarVisible then
-                imgui.SetColumnWidth(0, windowWidth - 42)
-            else
-                imgui.SetColumnWidth(0, windowWidth - 42 - scrollbarSize)
+--------------------------------
+-----[       Tabs        ]------
+--------------------------------
+local function renderTheming()
+    if imgui.BeginChild1("Colors", imgui.ImVec2(0, imgui.GetWindowHeight() - 100), false) then
+        for _, setting in pairs(sortedSettings.colors) do
+            if #setting.name > longestSettingName then
+                longestSettingName = #setting.name
             end
+            -- All are colors, create text and then 3 sliders
+            local color = ffi.new("float[4]", setting.tab.x, setting.tab.y, setting.tab.z, 1)
 
-            imgui.TextWrapped(message.message)
-
-            if scrollToBottom or forceBottom then
-                imgui.SetScrollHereY(1)
+            imgui.Text(toTitleCase(setting.name))
+            imgui.SameLine()
+            imgui.SetCursorPosX(longestSettingName * 8 + 10)
+            if imgui.ColorEdit3("##" .. setting.name, color, imgui.ColorEditFlags_NoInputs) then
+                UI.settings.colors[setting.name] = imgui.ImVec4(color[0], color[1], color[2], 1)
+                setting.tab = imgui.ImVec4(color[0], color[1], color[2], 1)
             end
-
-            imgui.NextColumn()
-            imgui.Text(os.date("%H:%M", message.sentTime))
-
-            imgui.Columns(1)
         end
 
         imgui.EndChild()
     end
 
-    if scrollToBottom then
-        scrollToBottom = false
+    imgui.SetCursorPosY(imgui.GetWindowHeight() - 32)
+    if imgui.Button("Reset to default") then
+        UI.settings = utils.copyTable(UI.defaultSettings)
+        sortedSettings = {}
+        local newSortedSettings = {}
+        for name, category in pairs(UI.defaultSettings) do
+            newSortedSettings[name] = {}
+            for settingName, setting in pairs(category) do
+                table.insert(newSortedSettings[name], {name = settingName, tab = setting})
+            end
+            table.sort(newSortedSettings[name], function(a, b) return a.name < b.name end)
+        end
+        sortedSettings = newSortedSettings
     end
 
-    imgui.PushStyleVar2(imgui.StyleVar_FramePadding, imgui.ImVec2(2, 2))
-    imgui.PushStyleVar2(imgui.StyleVar_ItemSpacing, imgui.ImVec2(2, 0))
-    imgui.PushStyleVar2(imgui.StyleVar_CellPadding, imgui.ImVec2(0, 0))
-    imgui.SetCursorPosY(imgui.GetWindowHeight() - 35)
+    imgui.SameLine()
 
-    imgui.PushStyleColor2(imgui.Col_FrameBg, imgui.ImVec4(UI.settings.colors.primaryColor.x, UI.settings.colors.primaryColor.y, UI.settings.colors.primaryColor.z, 1))
+    if imgui.Button("Save") then
+        saveConfig()
+    end
+end
 
-    if imgui.BeginChild1("ChatInput", imgui.ImVec2(0, 30), false) then
-        imgui.SetNextItemWidth(imgui.GetWindowWidth() - 25)
-        if imgui.InputText("##ChatInputMessage", chatMessageBuf, 256, imgui.InputTextFlags_EnterReturnsTrue + imgui.InputTextFlags_CallbackHistory, inputCallbackC) then
-            sendChatMessage(chatMessageBuf)
-            imgui.SetKeyboardFocusHere(1)
+local function renderGeneral()
+    if imgui.BeginChild1("General", imgui.ImVec2(0, imgui.GetWindowHeight() - 100), false) then
+        -- Inactive Fade
+        imgui.Text("Inactive fade")
+        imgui.SameLine()
+        imgui.SetCursorPosX(longestSettingName * 8 + 10)
+        local pInactiveFade = imgui.BoolPtr(UI.settings.window.inactiveFade)
+        if imgui.Checkbox("##Inactive fade", pInactiveFade) then
+            UI.settings.window.inactiveFade = pInactiveFade[0]
+        end
+
+        -- Fade Time
+        imgui.Text("Fade time")
+        imgui.SameLine()
+        imgui.SetCursorPosX(longestSettingName * 8 + 10)
+        local pFadeTime = imgui.FloatPtr(UI.settings.window.fadeTime)
+        if imgui.InputFloat("##Fade time", pFadeTime, 0.1, 1, "%.1f") then
+            if pFadeTime[0] < 0.1 then
+                pFadeTime[0] = 0.1
+            end
+
+            UI.settings.window.fadeTime = pFadeTime[0]
+        end
+
+        -- Fade when collapsed
+        imgui.Text("Fade when collapsed")
+        imgui.SameLine()
+        imgui.SetCursorPosX(longestSettingName * 8 + 10)
+        local pFadeWhenCollapsed = imgui.BoolPtr(UI.settings.window.fadeWhenCollapsed)
+        if imgui.Checkbox("##Fade when collapsed", pFadeWhenCollapsed) then
+            UI.settings.window.fadeWhenCollapsed = pFadeWhenCollapsed[0]
+        end
+
+        imgui.EndChild()
+
+        imgui.SetCursorPosY(imgui.GetWindowHeight() - 32)
+        if imgui.Button("Reset to default") then
+            UI.settings = utils.copyTable(UI.defaultSettings)
+            sortedSettings = {}
+            local newSortedSettings = {}
+            for name, category in pairs(UI.defaultSettings) do
+                newSortedSettings[name] = {}
+                for settingName, setting in pairs(category) do
+                    table.insert(newSortedSettings[name], {name = settingName, tab = setting})
+                end
+                table.sort(newSortedSettings[name], function(a, b) return a.name < b.name end)
+            end
+            sortedSettings = newSortedSettings
         end
 
         imgui.SameLine()
-        if utils.imageButton(UI.uiIcons.send.texId, 20) then
-            sendChatMessage(chatMessageBuf)
-            imgui.SetKeyboardFocusHere(1)
+
+        if imgui.Button("Save") then
+            saveConfig()
         end
-
-        imgui.EndChild()
-    end
-
-    imgui.PopStyleColor(1)
-    imgui.PopStyleVar(3)
-
-    if wasMessageSent then
-        heightOffset = 40
-
-        if not forceBottom then
-            imgui.SetCursorPos(imgui.ImVec2(imgui.GetWindowWidth() - (scrollbarVisible and scrollbarSize or 0) - 24, imgui.GetWindowHeight() - 60))
-            if utils.imageButton(UI.uiIcons.down.texId, 16) then
-                scrollToBottom = true
-                wasMessageSent = false
-            end
-        end
-    else
-        heightOffset = 20
     end
 end
 
+local tabs = {
+    theming = {
+        name = "Theming",
+        render = renderTheming,
+        id = 1,
+    },
+    general = {
+        name = "General",
+        render = renderGeneral,
+        id = 2,
+    }
+}
+
+local renderTab = renderTheming
+
+local function render()
+    imgui.PushStyleVar1(imgui.StyleVar_FrameRounding, 0)
+    imgui.Separator()
+
+    for _, tab in pairs(tabs) do
+        if imgui.Button(tab.name, imgui.ImVec2(imgui.GetWindowWidth() / 2, 23)) then
+            renderTab = tab.render
+        end
+        imgui.SameLine()
+    end
+
+    imgui.SetCursorPosY(66)
+    imgui.Separator()
+    imgui.SetCursorPosY(70)
+
+    renderTab()
+end
+
+local function onInit(settings)
+    sortedSettings = {} -- for reloading
+
+    -- Sort tabs by id
+    local newSortedTabs = {}
+    for _, tab in pairs(tabs) do
+        table.insert(newSortedTabs, tab)
+    end
+    table.sort(newSortedTabs, function(a, b) return a.id < b.id end)
+    tabs = newSortedTabs
+
+    -- Sort settings alphabetically
+    local newSortedSettings = {}
+    for name, category in pairs(settings) do
+        newSortedSettings[name] = {}
+        for settingName, setting in pairs(category) do
+            table.insert(newSortedSettings[name], {name = settingName, tab = setting})
+        end
+        table.sort(newSortedSettings[name], function(a, b) return a.name < b.name end)
+    end
+    sortedSettings = newSortedSettings
+end
+
 M.render = render
-M.sendChatMessage = sendChatMessage
-M.addMessage = addMessage
-M.clearHistory = clearHistory
+M.onInit = onInit
 
 return M

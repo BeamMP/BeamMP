8e81fabeac93a48b2f8f908df7a6b59509ad9bbf
Starystars67 <starystars67@gmail.com>
2023-11-06 17:23:41 +0000

    Added comments to MPElectricsGE

--- lua/ge/extensions/MPElectricsGE.lua
+++ lua/ge/extensions/MPElectricsGE.lua
@@ -12,7 +12,7 @@ local M = {}
 local lastElectrics
 
 
-
+--- Called on specified interval by MPUpdatesGE to simulate our own tick event to collect data.
 local function tick() -- Update electrics values of all vehicles
 	local ownMap = MPVehicleGE.getOwnMap() -- Get map of own vehicles
 	for i,v in pairs(ownMap) do -- For each own vehicle
@@ -25,8 +25,11 @@ end
 
 
 
-
-local function sendElectrics(data, gameVehicleID) -- Called by vehicle lua
+--- This function is called by VE lua to send the data from VE to GE and then to the server.
+--- INTERNAL USE
+--- @param data table The electrics data from VE
+--- @param gameVehicleID number The vehicle ID according to the local game
+local function sendElectrics(data, gameVehicleID)
 	if MPGameNetwork.launcherConnected() then
 		local serverVehicleID = MPVehicleGE.getServerVehicleID(gameVehicleID) -- Get serverVehicleID
 		if serverVehicleID and MPVehicleGE.isOwn(gameVehicleID) and data ~= lastElectrics then -- If serverVehicleID not null and player own vehicle
@@ -37,7 +40,9 @@ local function sendElectrics(data, gameVehicleID) -- Called by vehicle lua
 end
 
 
-
+--- This funct serves to move the electrics data from GE to VE for application
+--- @param data table The data to be applied as electrics
+--- @param serverVehicleID string The VehicleID according to the server.
 local function applyElectrics(data, serverVehicleID)
 	local gameVehicleID = MPVehicleGE.getGameVehicleID(serverVehicleID) or -1 -- get gameID
 	local veh = be:getObjectByID(gameVehicleID)
@@ -49,7 +54,8 @@ local function applyElectrics(data, serverVehicleID)
 end
 
 
-
+--- The raw message from the server. This is unpacked first and then sent to be applied according to message code.
+--- @param rawData string The raw message data.
 local function handle(rawData)
 	--print("MPElectricsGE.handle: "..rawData)
 	local code, serverVehicleID, data = string.match(rawData, "^(%a)%:(%d+%-%d+)%:({.*})")

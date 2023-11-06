5f4122798fcf87b5467a30340abbc43eeb19ad41
Starystars67 <starystars67@gmail.com>
2023-11-06 18:03:24 +0000

    Added comments for MPPowertrainGE

--- lua/ge/extensions/MPPowertrainGE.lua
+++ lua/ge/extensions/MPPowertrainGE.lua
@@ -3,12 +3,17 @@
 -- You have no permission to edit, redistribute or upload. Contact BeamMP for more info!
 --====================================================================================
 
+--- MPPowertrainGE API.
+--- Author of this documentation is Titch
+--- @module MPPowertrainGE
+--- @usage applyLivePowertrain(...) -- internal access
+--- @usage MPPowertrainGE.handle(...) -- external access
 
 
 local M = {}
 
 
-
+--- Called on specified interval by MPUpdatesGE to simulate our own tick event to collect data.
 local function tick()
 	local ownMap = MPVehicleGE.getOwnMap() -- Get map of own vehicles
 	for i,v in pairs(ownMap) do -- For each own vehicle
@@ -20,7 +25,10 @@ local function tick()
 end
 
 
-
+--- This function is called by VE lua to send the data from VE to GE and then to the server.
+--- INTERNAL USE
+--- @param data table The powertrain data from VE
+--- @param gameVehicleID number The vehicle ID according to the local game
 local function sendLivePowertrain(data, gameVehicleID)
 	if MPGameNetwork.launcherConnected() then
 		local serverVehicleID = MPVehicleGE.getServerVehicleID(gameVehicleID) -- Get serverVehicleID
@@ -31,7 +39,9 @@ local function sendLivePowertrain(data, gameVehicleID)
 end
 
 
-
+--- This function serves to send the powertrain data from GE to VE for application
+--- @param data table The data to be applied as powertrain
+--- @param serverVehicleID string The VehicleID according to the server.
 local function applyLivePowertrain(data, serverVehicleID)
 	local gameVehicleID = MPVehicleGE.getGameVehicleID(serverVehicleID) or -1 -- get gameID
 	local veh = be:getObjectByID(gameVehicleID)
@@ -41,7 +51,8 @@ local function applyLivePowertrain(data, serverVehicleID)
 end
 
 
-
+--- The raw message from the server. This is unpacked first and then sent to be applied according to message code.
+--- @param rawData string The raw message data.
 local function handle(rawData)
 	local code, serverVehicleID, data = string.match(rawData, "^(%a)%:(%d+%-%d+)%:({.*})")
 	if code == "l" then

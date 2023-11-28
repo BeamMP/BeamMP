8f98495b39403e6320bdbea0ed1863bcd01f0e47
NeotericGamer98 <74558635+NeotericGamer98@users.noreply.github.com>
2023-11-28 18:03:26 -0500

    Updated BeamMP-Chat
    Updated the app.html styling to NOT look like Discord. I did my best to make it look as unobtrusive as possible.
    
    Updated app.js and refined a couple of functions. Auto-Scroll and swap functions seem to work fine.
    
    Updated app.json slightly.
    
    Added app.css as a backup file.

--- ui/modules/apps/BeamMP-Chat/app.js
+++ ui/modules/apps/BeamMP-Chat/app.js
@@ -2,6 +2,7 @@ var app = angular.module('beamng.apps');
 let lastSentMessage = "";
 let lastMsgId = 0;
 let newChatMenu = false;
+
 app.directive('multiplayerchat', [function () {
 	return {
 		templateUrl: '/ui/modules/apps/BeamMP-Chat/app.html',
@@ -13,25 +14,33 @@ app.directive('multiplayerchat', [function () {
 }]); 
 
 app.controller("Chat", ['$scope', 'Settings', function ($scope, Settings) {
-	$scope.init = function() {
+
+	$scope.init = function () {
+
 		var chatMessages = retrieveChatMessages()
 		newChatMenu = Settings.values.enableNewChatMenu;
+
 		//console.log(`[CHAT] New chat menu: ${newChatMenu}`);
+
 		// Set listeners
 		var chatinput = document.getElementById("chat-input");
+
 		// To ensure that the element exists
 		if (chatinput) {
-			chatinput.addEventListener("mouseover", function(){ chatShown = true; showChat(); });
-			chatinput.addEventListener("mouseout", function(){ chatShown = false; });
+			chatinput.addEventListener("mouseover", function () { chatShown = true; showChat(); });
+			chatinput.addEventListener("mouseout", function () { chatShown = false; });
 			chatinput.addEventListener('keydown', onKeyDown); //used for 'up arrow' last msg functionality
+			chatinput.addEventListener('keydown', onEnterKeyPress); // Handles Enter key press
 		}
 
 		var chatlist = document.getElementById("chat-list");
+
 		// To ensure that the element exists
 		if (chatlist) {
 			chatlist.addEventListener("mouseover", function(){ chatShown = true; showChat(); });
 			chatlist.addEventListener("mouseout", function(){ chatShown = false; });
 		}
+
 		// Set chat direction
 		setChatDirection(localStorage.getItem('chatHorizontal'));
 		setChatDirection(localStorage.getItem('chatVertical'));
@@ -58,59 +67,102 @@ app.controller("Chat", ['$scope', 'Settings', function ($scope, Settings) {
 		bngApi.engineLua('setCEFFocus(true)');
 	};
 
-	// Set chat direction.
+	// Function to handle Enter key press
+	function onEnterKeyPress(event) {
+		const chatInput = document.getElementById("chat-input");
+		if (event.key === "Enter" && chatInput === document.activeElement) {
+			event.preventDefault();
+			const text = chatInput.value;
+			if (text.trim() !== "") {
+				$scope.chatSend();
+			}
+			chatInput.value = '';
+		}
+	}
+
+	// Set chat direction
 	function setChatDirection(direction) {
-		const chatbox = document.getElementById("chatbox");
-		const chatwindow = document.getElementById("chat-window");
-		const chatlist = document.getElementById("chat-list");
-		if (direction == "left") {
-			chatbox.style.flexDirection = "row";
-			chatbox.style.marginLeft = "0px";
-			chatwindow.style.alignItems = "flex-start";
-			localStorage.setItem('chatHorizontal', "left");
-		}
-		else if (direction == "right") {
-			chatbox.style.flexDirection = "row-reverse";
-			chatbox.style.marginLeft = "auto";
-			chatwindow.style.alignItems = "flex-start";
-			localStorage.setItem('chatHorizontal', "right");
-		}
-		else if (direction == "middle") {
-			chatbox.style.flexDirection = "row";
-			chatbox.style.marginLeft = "0px";
-			chatwindow.style.alignItems = "center";
-			localStorage.setItem('chatHorizontal', "middle");
-		}
-		else if (direction == "top") {
-			chatwindow.style.flexDirection = "column-reverse";
-			chatlist.style.flexDirection = "column-reverse";
-			chatlist.style.marginTop = "0px";
-			chatlist.style.marginBottom = "auto";
-			localStorage.setItem('chatVertical', "top");
-		}
-		else if (direction == "bottom") {
-			chatwindow.style.flexDirection = "column";
-			chatlist.style.flexDirection = "column";
-			chatlist.style.marginTop = "auto";
-			chatlist.style.marginBottom = "0px";
-			localStorage.setItem('chatVertical', "bottom");
+	const chatbox = document.getElementById("chatbox");
+	const chatwindow = document.getElementById("chat-window");
+	const chatlist = document.getElementById("chat-list");
+
+		switch (direction) {
+			case "left":
+				chatbox.style.flexDirection = "row";
+				chatbox.style.marginLeft = "0";
+				chatwindow.style.alignItems = "flex-start";
+				localStorage.setItem('chatHorizontal', "left");
+				break;
+
+			case "right":
+				chatbox.style.flexDirection = "row-reverse";
+				chatbox.style.marginLeft = "auto";
+				chatwindow.style.alignItems = "flex-start";
+				localStorage.setItem('chatHorizontal', "right");
+				break;
+
+			case "middle":
+				chatbox.style.flexDirection = "row";
+				chatbox.style.marginLeft = "0";
+				chatwindow.style.alignItems = "center";
+				localStorage.setItem('chatHorizontal', "middle");
+				break;
+
+			case "top":
+				chatwindow.style.flexDirection = "column-reverse";
+				chatlist.style.flexDirection = "column-reverse";
+				chatlist.style.marginTop = "0";
+				chatlist.style.marginBottom = "auto";
+				localStorage.setItem('chatVertical', "top");
+				break;
+
+			case "bottom":
+				chatwindow.style.flexDirection = "column";
+				chatlist.style.flexDirection = "column";
+				chatlist.style.marginTop = "auto";
+				chatlist.style.marginBottom = "0";
+				localStorage.setItem('chatVertical', "bottom");
+				break;
+
+			default:
+				// Handle default behavior or error case
+				break;
 		}
 	}
 
-	// Swap chat direction. (left/middle/right)
-	$scope.chatSwapHorizontal = function() {
+	// Swap chat direction (left/middle/right)
+	$scope.chatSwapHorizontal = function () {
 		const chatHorizontal = localStorage.getItem('chatHorizontal') || "middle";
-		if (chatHorizontal == "left") setChatDirection("middle");
-		else if (chatHorizontal == "middle") setChatDirection("right");
-		else setChatDirection("left");
-	}
 
-	// Swap chat direction. (top/bottom)
-	$scope.chatSwapVertical = function() {
-		const chatVertical = localStorage.getItem('chatVertical');
-		if (chatVertical != "top") setChatDirection("top");
-		else setChatDirection("bottom");
-	}
+		switch (chatHorizontal) {
+			case "left":
+				setChatDirection("middle");
+				break;
+
+			case "middle":
+				setChatDirection("right");
+				break;
+
+			default:
+				setChatDirection("left");
+				break;
+		}
+	};
+
+	// Swap chat direction (top/bottom)
+	$scope.chatSwapVertical = function () {
+		const chatVertical = localStorage.getItem('chatVertical') || "";
+
+		switch (chatVertical) {
+			case "top":
+				setChatDirection("bottom");
+				break;
+
+			default:
+				setChatDirection("top");
+				break;
+		}
+	};
 
 	//Chat message function.
 	$scope.$on('chatMessage', function (event, data) {
@@ -137,7 +189,7 @@ app.controller("Chat", ['$scope', 'Settings', function ($scope, Settings) {
 		localStorage.removeItem('chatMessages');
 	})
 
-	//Settings changed function.
+	// Settings changed function
 	$scope.$on('SettingsChanged', function (event, data) {
 		Settings.values = data.values;
 		const chatbox = document.getElementById("chat-window");
@@ -148,19 +200,21 @@ app.controller("Chat", ['$scope', 'Settings', function ($scope, Settings) {
 		}
 	})
 
-	//Chat send function.
-	$scope.chatSend = function() {
+	// Chat Send Function
+	$scope.chatSend = function () {
 		let chatinput = document.getElementById("chat-input");
-		const text = chatinput.value
+		const text = chatinput.value;
 		if (text) {
 			lastSentMessage = text;
 			if (text.length > 500) addMessage("Your message is over the character limit! (500)");
 			else {
 				bngApi.engineLua('UI.chatSend(' + bngApi.serializeToLua(text) + ')');
 				chatinput.value = '';
+				chatinput.blur();
 			}
 		}
 	};
+
 }]);
 
 // --CHAT FADING --//
@@ -199,32 +253,24 @@ async function fadeNode(node) {
 async function showChat() {
 	if (newChatMenu) return;
 
-	// While the mouse is over the chat, we wait
-	var chatMessages = []
+	const chatList = document.getElementById("chat-list");
+	const chatMessages = Array.from(chatList.getElementsByTagName("li"));
+
 	while (chatShown) {
-		// Get the chat and the messages
-		// Copy the variables so it's a pointer
-		var tempMessages = document.getElementById("chat-list").getElementsByTagName("li");
-		for (i = 0; i < tempMessages.length; i++) {
-			chatMessages[i] = tempMessages[i];
-		}
-		// Set all messages opacity to 1.0
-		for (var i = 0; i < chatMessages.length; ++i) chatMessages[i].style.opacity = 1.0;
+		chatMessages.forEach(message => message.classList.add("show-chat"));
 		await sleep(100);
 	}
-	// Once the mouse is not over the chat anymore, we wait before fading
-	// We take care of checking that the chat is not shown while we are waiting before fading
-	for (var steps = chatShowTime/35; steps < chatShowTime; steps += chatShowTime/35) {
+
+	for (let steps = chatShowTime / 35; steps < chatShowTime; steps += chatShowTime / 35) {
 		if (chatShown) return;
-		await sleep(chatShowTime/35);
+		await sleep(chatShowTime / 35);
 	}
-	var chatOpacity = 1.0;
+
+	let chatOpacity = 1.0;
 	while (chatOpacity > 0.0) {
-		// If the user move the mouse hover the chat before
-		// this loop as ended then we break the loop
 		if (chatShown) break;
-		chatOpacity = chatOpacity - chatFadeSteps;
-		for (var i = 0; i < chatMessages.length; ++i) chatMessages[i].style.opacity = chatOpacity;
+		chatMessages.forEach(message => message.classList.remove("show-chat"));
+		chatOpacity -= chatFadeSteps;
 		await sleep(chatFadeSpeed);
 	}
 }
@@ -333,56 +379,59 @@ function retrieveChatMessages() {
 	}
 }
 
-// Max number of messages to display in the chat window.
+//Max number of messages.
 const MAX_MESSAGES = 100;
 
 //Add a message to the chat window.
 function addMessage(msg, time = null) {
-
-	//Get user's time and format it to HH:MM:SS using the user's timezone and locale. Then add it to the message.
 	const currentTime = time || new Date();
 	const formattedTime = new Intl.DateTimeFormat(undefined, {
 		hour: "numeric",
-        minute: "numeric",
-        second: "numeric",
-        hour12: false,
+		minute: "numeric",
+		second: "numeric",
+		hour12: false,
 	}).format(currentTime);
 
-	const msgText = "" + msg;
-	const messageWithTime = formattedTime + "" + msg;
+	const chatList = document.getElementById("chat-list");
+	const chatMessageNode = createMessageNode(msg, formattedTime);
+	appendMessageToChat(chatList, chatMessageNode);
 
-	// Create the message node.
+	// Ensure the chat window is scrolled to the bottom after adding a message
+	scrollChat(chatList);
+}
+
+//Create a chat message node.
+function createMessageNode(msg, formattedTime) {
 	const chatMessageNode = document.createElement("li");
 	chatMessageNode.className = "chat-message";
 	fadeNode(chatMessageNode);
 
-	// Create node for the timestamp.
 	const messageTimestampNode = document.createElement("span");
 	messageTimestampNode.className = "chat-message-timestamp";
 	const timestampTextNode = document.createTextNode(formattedTime);
 	messageTimestampNode.appendChild(timestampTextNode);
-	chatMessageNode.appendChild(messageTimestampNode)
+	chatMessageNode.appendChild(messageTimestampNode);
 
-	// create text for the message itself, add it to chat message list
-	const chatList = document.getElementById("chat-list");
-
-	// Check if this message is a server message before doing rich formatting.
+	const msgText = "" + msg;
 	if (msgText.startsWith("Server: ")) {
 		const formattedInnerHtml = formatRichString(msgText);
-		chatMessageNode.innerHTML = chatMessageNode.innerHTML + formattedInnerHtml;
+		chatMessageNode.innerHTML += formattedInnerHtml;
 	} else {
 		const textNode = document.createTextNode(msgText);
 		chatMessageNode.appendChild(textNode);
 	}
 
+	return chatMessageNode;
+}
+
+//Append a message to the chat window.
+function appendMessageToChat(chatList, chatMessageNode) {
 	chatList.appendChild(chatMessageNode);
 
-	// Delete oldest chat message if more than 100 messages exist.
+	// Remove oldest message if the number of messages exceeds the maximum limit
 	if (chatList.children.length > MAX_MESSAGES) {
 		chatList.removeChild(chatList.children[0]);
 	}
-
-	scrollChat(chatList);
 }
 
 //Scroll chat window to bottom.
@@ -397,9 +446,9 @@ function scrollChat(chatList) {
     
 // --KEYBOARD SHORTCUTS --//
 function onKeyDown(e) {
-	if (e.key == "ArrowUp") {
-		console.log(e);
-		document.getElementById("chat-input").value = lastSentMessage;
-		e.target.setSelectionRange(lastSentMessage.length, lastSentMessage.length);
+	const chatInput = document.getElementById("chat-input");
+	if (e.key === "ArrowUp") {
+		chatInput.value = lastSentMessage;
+		chatInput.setSelectionRange(lastSentMessage.length, lastSentMessage.length);
 	}
 }

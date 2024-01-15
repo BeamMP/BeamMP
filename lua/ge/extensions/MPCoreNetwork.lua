50bb246dc22f548824f38360025f64a409d99005
Lion Kortlepel <development@kortlepel.com>
2024-01-15 14:40:52 +0100

    add copyright header to lua files

--- lua/ge/extensions/MPCoreNetwork.lua
+++ lua/ge/extensions/MPCoreNetwork.lua
@@ -1,8 +1,20 @@
---====================================================================================
--- All work by Titch2000, jojos38 & 20dka.
--- You have no permission to edit, redistribute or upload other than for the purposes of contributing. 
--- Contact BeamMP for more info!
---====================================================================================
+-- BeamMP, the BeamNG.drive multiplayer mod.
+-- Copyright (C) 2024 BeamMP Ltd., BeamMP team and contributors.
+--
+-- BeamMP Ltd. can be contacted by electronic mail via contact@beammp.com.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published
+-- by the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 --- MPCoreNetwork API. Handles Main Launcher <-> Game Network. Version Check, Server list transfer, login, connect to X server, quiting a server etc.
 --- Author of this documentation is Titch
--- lua/ge/extensions/MPModManager.lua
+++ lua/ge/extensions/MPModManager.lua
@@ -1,7 +1,20 @@
---====================================================================================
--- All work by Titch2000 and jojos38.
--- You have no permission to edit, redistribute or upload. Contact BeamMP for more info!
---====================================================================================
+-- BeamMP, the BeamNG.drive multiplayer mod.
+-- Copyright (C) 2024 BeamMP Ltd., BeamMP team and contributors.
+--
+-- BeamMP Ltd. can be contacted by electronic mail via contact@beammp.com.
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU Affero General Public License as published
+-- by the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU Affero General Public License for more details.
+--
+-- You should have received a copy of the GNU Affero General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 --- MPModManager API.
 --- Author of this documentation is Titch

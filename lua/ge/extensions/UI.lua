a81e9aec6b9b2a58f8759353132bc88c0c0f3901
Tixx <83774803+WiserTixx@users.noreply.github.com>
2024-08-24 23:04:14 +0200

    Fix notifications overwriting all messages and add new params

--- lua/ge/extensions/UI.lua
+++ lua/ge/extensions/UI.lua
@@ -207,7 +207,9 @@ end
 --- Display a prompt in the top corner as a notification, Good for server related events like joins/leaves
 -- @param text string
 -- @param type string
-local function showNotification(text, type)
+-- @param category string 
+-- @param icon string material_ icons from ui\assets\Sprites\svg-symbols.svg example: smoking_rooms
+local function showNotification(text, type, category, icon)
 	if type and type == "error" then
 		log('I', 'showNotification', "[UI Error] > "..tostring(text))
 	else
@@ -217,7 +219,7 @@ local function showNotification(text, type)
 		--local joinedName = string.match(text, "^Welcome (.+)!$")
 		--if joinedName then MPVehicleGE.onPlayerJoined(joinedName) end
 	end
-	ui_message(''..text, 10, nil, nil)
+	ui_message(''..text, 10, category or text, icon)
 end
 --- Show a UI dialog / alert box to inform the user of something.
 -- @param options any

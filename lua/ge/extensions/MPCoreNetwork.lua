a7a09ca60226f43eb93422ab2e574da441112530
Tixx <83774803+WiserTixx@users.noreply.github.com>
2024-09-17 22:40:58 +0200

    Move handling of warning packet to its own function

--- lua/ge/extensions/MPCoreNetwork.lua
+++ lua/ge/extensions/MPCoreNetwork.lua
@@ -489,6 +489,14 @@ local function promptAutoJoin(params)
 	UI.promptAutoJoinConfirmation(params)
 end
 
+local function handleModWarning(params)
+	if params == 'MODS_FOUND' and settings.getValue("skipModSecurityWarning", false) == false then
+		guihooks.trigger('DownloadSecurityPrompt', params) 
+	else 
+		send('WY') 
+	end
+end
+
 -- VV============= EVENTS =============VV
 
 --- Handle network message events.
@@ -503,7 +511,7 @@ local HandleNetwork = {
 	['M'] = function(params) log('W', 'HandleNetwork', 'Received Map! '..params) loadLevel(params) end,
 	['N'] = function(params) loginReceived(params) end,
 	['U'] = function(params) handleU(params) end, -- Loading into server UI, handles loading mods, pre-join kick messages and ping
-	['W'] = function(params) if params == 'MODS_FOUND' and settings.getValue("skipModSecurityWarning", false) == false then guihooks.trigger('DownloadSecurityPrompt', params) else send('WY') end end,
+	['W'] = function(params) handleModWarning(params) end,
 	['Z'] = function(params) launcherVersion = params; end,
 }
 

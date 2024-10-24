9b6dbb60deb1e6a2a64564005321f3366a0fa737
Starystars67 <starystars67@gmail.com>
2024-10-24 16:55:13 +0100

    Bump version number

--- lua/ge/extensions/MPCoreNetwork.lua
+++ lua/ge/extensions/MPCoreNetwork.lua
@@ -19,7 +19,7 @@ local socket = require('socket')
 local launcherConnected = false
 local isConnecting = false
 local launcherVersion = "" -- used only for the server list
-local modVersion = "4.13.5" -- the mod version
+local modVersion = "4.13.6" -- the mod version
 -- server
 
 local serverList -- server list JSON

5ab21fc6c8670f11b0b8bccfb68f7dc98157fc6d
Preston Peek <36671440+dprestonpeek@users.noreply.github.com>
2025-03-05 12:10:14 -0500

    Fix French and German translations
    Also cleaned up some dev files

--- Deploy-BeamMP.bat
+++
@@ -1,5 +0,0 @@
-del /f BeamMP.zip
-"C:\Program Files\7-Zip\7z.exe" -r a BeamMP.zip . -xr!*.git* -xr!*.github* -xr!*.idea* -xr!*3rd_party_licenses* -xr!*Deploy-BeamMP.bat* || pause && exit
-
-del /f C:\Users\dpres\AppData\Local\BeamNG.drive\0.34\mods\multiplayer\BeamMP.zip || pause && exit
-echo F | xcopy /Y /S BeamMP.zip C:\Users\dpres\AppData\Local\BeamNG.drive\0.34\mods\multiplayer\BeamMP.zip || pause && exit

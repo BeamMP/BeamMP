db639378b2592d531526da2c82648f7505845569
Starystars67 <starystars67@gmail.com>
2026-08-02 14:41:46 +0100

    Add text styling back to server names and add support into chat and session bar

--- ui/ui-vue/mods/BeamMP/shared/constants.js
+++ ui/ui-vue/mods/BeamMP/shared/constants.js
@@ -7,3 +7,28 @@ export const BEAMMP_TILES_ROUTE_NAME = "menu.beammp.tiles"
 export const BEAMMP_SERVERS_ROUTE_NAME = "menu.beammp.servers"
 export const BEAMMP_CURRENT_SERVER_ROUTE_NAME = "menu.beammp.current-server"
 export const BEAMMP_DIRECT_ROUTE_NAME = "menu.beammp.direct"
+
+export const BEAMMP_TEXT_STYLE_MAP = {
+  '^0': 'color-0',
+  '^1': 'color-1',
+  '^2': 'color-2',
+  '^3': 'color-3',
+  '^4': 'color-4',
+  '^5': 'color-5',
+  '^6': 'color-6',
+  '^7': 'color-7',
+  '^8': 'color-8',
+  '^9': 'color-9',
+  '^a': 'color-a',
+  '^b': 'color-b',
+  '^c': 'color-c',
+  '^d': 'color-d',
+  '^e': 'color-e',
+  '^f': 'color-f',
+  '^l': 'bold',
+  '^m': 'line-through',
+  '^n': 'underline',
+  '^o': 'italic',
+}
+
+globalThis.beammpTextStyleMap = BEAMMP_TEXT_STYLE_MAP

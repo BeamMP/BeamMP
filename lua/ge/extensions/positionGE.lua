f29b5cf0cc052b57974e7798b203f2f17ff10d51
Olrosse <10835721+Olrosse@users.noreply.github.com>
2026-08-15 18:17:07 +0200

    removed position smoother
    times sync is a more reliable solution and the smoother was potentially causing prediction errors

--- lua/ge/extensions/positionGE.lua
+++ lua/ge/extensions/positionGE.lua
@@ -14,27 +14,6 @@ local M = {}
 local targetGameSpeed = 1
 local actualSimSpeed = 1
 
---[[
-	["X-Y"] = table
-		[data] = table
-			[pos] = array[3]
-			[rot] = array[4]
-			[vel] = array[3]
-			[rvel] = array[4]
-			[tim] = float
-			[ping] = float
-		[last_executed_tim] = float
-		[executed_last] = hptimerstruct
-		[median] = float
-		[median_array] = array
-			[1] = next index
-			[2] = max array buffer size
-			[3..[2] + 2] = float
-		[median_timer] = hptimerstruct
-		[executed] = bool
-]]
-local POSSMOOTHER = {}
-local TIMER = (HighPerfTimer or hptimer) -- game own timer that is much more accurate then os.clock()
 
 
 
@@ -96,74 +75,6 @@ local function applyPos(data, serverVehicleID)
 	end
 end
 
---- Tries to delay the positional update execution to match the average update interval from this vehicle
--- Reduces vehicle warping
--- @tparam serverVehicleID string X-Y
--- @tparam decoded table The data to be applied to a vehicle, needs to contain "pos", "rot", "vel", "rvel", "ping" and "tim"
-local function smoothPosExec(serverVehicleID, decoded)
-	--[[ Alternate idea
-		Buffer the unexecuted received packets in a by tim sorted table
-			[0] = packet
-			[1] = packet
-			[2] = packet
-		Tick at a specific interval (eg every 22 miliseconds)
-		Look at the buffer of packets, take packet that is closest to it.
-		If we want to exec a packet with tim 0.044 but we only have 0.022 and 0.066
-		then produce the 0.044 packet from those two. We need to calc where the car would be in relation of these two packets, not just make a median of two packets. This Idea needs to be thought through.
-	]]
-	if POSSMOOTHER[serverVehicleID] == nil then
-		local new = {}
-		new.data = decoded
-		new.last_executed_tim = decoded.tim
-		new.executed_last = TIMER()
-		new.executed = false
-		new.median = 32
-		new.median_array = {3,10,32,32,32,32,32,32,32,32,32,32}
-		--new.median_array = {3,20,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32}
-		new.median_timer = TIMER()
-		POSSMOOTHER[serverVehicleID] = new
-				
-	elseif decoded.tim < 3 or (POSSMOOTHER[serverVehicleID].last_executed_tim - decoded.tim) > 3 then -- if remote timer got reset or if new data is 3 seconds earlier then the known, expect that the remote vehicle got reset.
-		POSSMOOTHER[serverVehicleID].data = decoded
-		POSSMOOTHER[serverVehicleID].last_executed_tim = decoded.tim
-		POSSMOOTHER[serverVehicleID].executed = false
-				
-	elseif POSSMOOTHER[serverVehicleID].last_executed_tim > decoded.tim then
-		-- nothing, outdated data
-		
-	else
-		-- notes
-		-- right order 0.022 -- 0.044 -- 0.066
-		-- wrong order 0.022 -- 0.066 -- 0.044 (if 0.066 is received first, we overwrite it with 0.044 -> if 0.066 wasnt executed yet. otherwise this wouldnt be reached)
-		-- Todo: When this happens, try to calc a median packet between the two for all relevant data. eg. (decoded.pos + POSSMOOTHER[serverVehicleID].data.pos) / 2POSSMOOTHER[serverVehicleID].data.pos) / 2
-		-- This likely proposes an issue if the tim values are to far away from each other.
-		
-		-- ensure that there is a min age distance between the remote packages of 15ms.
-		if (decoded.tim - POSSMOOTHER[serverVehicleID].last_executed_tim) < 0.015 then return nil end
-		
-		local median_time = POSSMOOTHER[serverVehicleID].median_timer:stopAndReset()
-		POSSMOOTHER[serverVehicleID].data = decoded -- also outdates unexecuted packets
-		POSSMOOTHER[serverVehicleID].executed = false
-		if median_time > 14 then -- there can be lower intervals then 32ms, so we cover that
-			if median_time < 80 then
-				local median_array = POSSMOOTHER[serverVehicleID].median_array
-				local next_index = median_array[1]
-				median_array[next_index] = median_time
-				median_array[1] = next_index + 1
-				if next_index == median_array[2] + 2 then
-					median_array[1] = 3
-					local median = 0
-					for i = 3, median_array[2] + 2 do
-						median = median + median_array[i]
-					end
-					-- median + X to artificially count in small fluctuations
-					POSSMOOTHER[serverVehicleID].median = (median / median_array[2]) + 3
-				end
-				POSSMOOTHER[serverVehicleID].median_array = median_array
-			end
-		end
-	end
-end
 
 --- The raw message from the server. This is unpacked first and then sent to applyPos() or smoothPosExec()
 -- @param rawData string The raw message data.
@@ -177,12 +88,7 @@ local function handle(rawData)
 	end
 
 	if code == 'p' then
-		if settings.getValue("enablePosSmoother") then
-			local decoded = jsonDecode(data)
-			smoothPosExec(serverVehicleID, decoded)
-		else
-			applyPos(data, serverVehicleID)
-		end
+		applyPos(data, serverVehicleID)
 	else
 		log('W', 'handle', "Received unknown packet '"..tostring(code).."'! ".. rawData)
 	end
@@ -247,23 +153,6 @@ local function getActualSimSpeed()
 	return actualSimSpeed
 end
 
---- This function is used to execute smoothed positional updates if enabled
-local function onPreRender(dt)
-	-- tick pos updates per vehicle based on their median pos update interval
-	for serverVehicleID, data in pairs(POSSMOOTHER) do
-		local timedif = data.executed_last:stop()
-		if not data.executed and timedif >= data.median then
-			POSSMOOTHER[serverVehicleID].executed_last:stopAndReset()
-			POSSMOOTHER[serverVehicleID].executed = true
-			POSSMOOTHER[serverVehicleID].last_executed_tim = data.data.tim
-			applyPos(jsonEncode(data.data), serverVehicleID)
-			
-		elseif timedif > 60000 then -- seconds. vehicle potentially removed. rem entry
-			POSSMOOTHER[serverVehicleID] = nil
-		end
-	end
-end
-
 local function onUpdate(dtReal, dtSim, dtRaw)
 	if MPGameNetwork and MPGameNetwork.launcherConnected() then
 		setActualSimSpeed(dtSim/dtRaw)
@@ -279,15 +168,6 @@ local function onUpdate(dtReal, dtSim, dtRaw)
 	end
 end
 
---- This function is used to reset the positional update smoother when it is disabled
-local function onSettingsChanged()
-	if not settings.getValue("enablePosSmoother") then -- nil/false
-		for serverVehicleID, _ in pairs(POSSMOOTHER) do
-			POSSMOOTHER[serverVehicleID] = nil
-		end
-	end
-end
-
 M.applyPos                    = applyPos
 M.tick                        = tick
 M.handle                      = handle
@@ -297,10 +177,7 @@ M.setPositionRotationVelocity = setPositionRotationVelocity
 M.setPing                     = setPing
 M.setActualSimSpeed           = setActualSimSpeed
 M.getActualSimSpeed           = getActualSimSpeed
-M.onPreRender                 = onPreRender
 M.onUpdate                    = onUpdate
-M.onSettingsChanged           = onSettingsChanged
-M.posSmoother                 = POSSMOOTHER -- debug entry
 M.onInit = function() setExtensionUnloadMode(M, "manual") end
 
 return M

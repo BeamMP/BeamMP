6deabb8e64e4ffe39ac8815f94fc05ee0263b644
Starystars67 <starystars67@gmail.com>
2026-09-11 01:23:56 +0100

    Fix downloaded UI locale

--- ui/ui-vue/mods/BeamMP/layouts/BeamMPMain.vue
+++ ui/ui-vue/mods/BeamMP/layouts/BeamMPMain.vue
@@ -110,7 +110,7 @@
             <div class="download-info">
               <div class="download-line">
                 <code>{{ mod.number }} - {{ mod.name }}</code>
-                <small>{{ index === 0 ? mod.speed : $tt("ui.beammp.download.downloaded") }}</small>
+                <small>{{ index === 0 ? mod.speed : $tt("ui.common.beammp.downloaded") }}</small>
               </div>
               <div v-if="index === 0 && mod.progress < 100" class="progress-track">
                 <span :style="{ width: `${Math.max(0, Math.min(100, mod.progress))}%` }" />

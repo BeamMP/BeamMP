df71b7cc682560b9f7856da49df035d950f5feaa
Tixx <83774803+WiserTixx@users.noreply.github.com>
2026-09-11 18:01:43 +0200

    Bump version to v4.22.4

--- lua/ge/extensions/MPCoreNetwork.lua
+++ lua/ge/extensions/MPCoreNetwork.lua
@@ -27,7 +27,7 @@ local isConnecting = false
 local proxyPort = ""
 local socketPartialData
 local launcherVersion = "" -- used only for the server list
-local modVersion = "4.22.3" -- the mod version
+local modVersion = "4.22.4" -- the mod version
 -- server
 
 local serverList -- server list JSON
